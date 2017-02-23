e2308894cc286355602be6a5dd12a34a669b3e61
Aliaksei Padabed <Aliaksei.Padabed@gmail.com>
2017-02-24 01:24:43 +0300

    + added Repository type

--- PVT.Q1.2017.Shop/PVT.Q1.2017.Shop.Tests/DataBaseTest.cs
+++ PVT.Q1.2017.Shop/PVT.Q1.2017.Shop.Tests/DataBaseTest.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Text;
 using System.Collections.Generic;
+using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Shop.Common.Models;
 using Shop.DAL.Context;
+using Shop.DAL.Repositories;
 
 namespace PVT.Q1._2017.Shop.Tests
 {
@@ -70,6 +73,85 @@ namespace PVT.Q1._2017.Shop.Tests
                 }
 
                 context.Database.Create();
+            }
+        }
+
+        [TestMethod]
+        public void AddModelTest()
+        {
+            string trackName = "Hello";
+            using (var context = new ShopContext())
+            {
+                var repository = new Repository<Track>(context);
+
+                repository.AddOrUpdate(new Track { Name = trackName });
+                context.SaveChanges();
+
+                Assert.IsTrue(context.Tracks.Any(t => t.Name == trackName));
+            }
+        }
+
+        [TestMethod]
+        public void GetAllModelWithExpressionTest()
+        {
+            string track1Name = "Track 1";
+            string track2Name = "Track 2";
+            string track3Name = "Track 3";
+
+            using (var context = new ShopContext())
+            {
+                var repository = new Repository<Track>(context);
+
+                repository.AddOrUpdate(new Track { Name = track1Name });
+                repository.AddOrUpdate(new Track { Name = track2Name });
+                repository.AddOrUpdate(new Track { Name = track3Name });
+                context.SaveChanges();
+
+                Assert.IsTrue(repository.GetAll(t => t.Name.StartsWith("Track")).Count >= 3);
+            }
+        }
+
+        [TestMethod]
+        public void GetAllModelWithoutExpressionTest()
+        {
+            string track1Name = "Track 1";
+            string track2Name = "Track 2";
+            string track3Name = "Track 3";
+
+            using (var context = new ShopContext())
+            {
+                var repository = new Repository<Track>(context);
+
+                repository.AddOrUpdate(new Track { Name = track1Name });
+                repository.AddOrUpdate(new Track { Name = track2Name });
+                repository.AddOrUpdate(new Track { Name = track3Name });
+                context.SaveChanges();
+
+                Assert.IsTrue(repository.GetAll().Count >= 3, repository.GetAll().Count.ToString());
+            }
+        }
+
+        [TestMethod]
+        public void UpdateModelTest()
+        {
+            string trackName = "Hello";
+            using (var context = new ShopContext())
+            {
+                var repository = new Repository<Track>(context);
+
+                repository.AddOrUpdate(new Track { Name = trackName });
+                context.SaveChanges();
+
+                var track = repository.GetAll(t => t.Name == trackName).FirstOrDefault();
+                Assert.IsNotNull(track);
+
+                var duration = new TimeSpan(0, 2, 46);
+                track.Duration = duration;
+
+                repository.AddOrUpdate(track);
+                context.SaveChanges();
+
+                Assert.IsTrue(context.Tracks.Any(t => t.Duration == duration));
             }
         }
     }
---
+++ PVT.Q1.2017.Shop/Shop.DAL/Repositories/Repository.cs
@@ -0,0 +1,179 @@
+﻿namespace Shop.DAL.Repositories
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Data.Entity;
+    using System.Linq;
+    using System.Linq.Expressions;
+    using Infrastructure.Models;
+    using Infrastructure.Repositories;
+
+    /// <summary>
+    /// The models repository.
+    /// </summary>
+    public class Repository<TEntity> : IRepository<TEntity>, IDisposable where TEntity : BaseEntity, new()
+    {
+        #region Fields
+
+        /// <summary>
+        /// The Db context.
+        /// </summary>
+        private readonly DbContext _dbContext;
+
+        #endregion //Fields
+
+        #region Constructors
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Repository{TEntity}"/> class.
+        /// </summary>
+        /// <param name="dbContext">
+        /// The Db context.
+        /// </param>
+        /// <exception cref="ArgumentNullException">
+        /// When <paramref name="dbContext"/> is null.
+        /// </exception>
+        public Repository(DbContext dbContext)
+        {
+            if (dbContext == null)
+            {
+                throw new ArgumentNullException(nameof(dbContext));
+            }
+
+            this._dbContext = dbContext;
+        }
+
+        #endregion //Constructors
+
+        #region IRepository<TEntity> Members
+
+        /// <summary>
+        /// Tries to find a model by the specified <paramref name="id"/>.
+        /// </summary>
+        /// <param name="id">
+        /// The model key.
+        /// </param>
+        /// <returns>
+        /// A model with the specified <paramref name="id"/> or null in case if there are now models with such <paramref name="id"/>.
+        /// </returns>
+        public TEntity GetById(int id)
+        {
+            var currentTable = this._dbContext.Set<TEntity>();
+            return currentTable.Find(id);
+        }
+
+        /// <summary>
+        /// Returns all models from the repository.
+        /// </summary>
+        /// <returns>
+        /// All models from the repository.
+        /// </returns>
+        public ICollection<TEntity> GetAll()
+        {
+            var currentTable = this._dbContext.Set<TEntity>();
+            return currentTable.ToList();
+        }
+
+        /// <summary>
+        /// Tries to find models from the repository using the specified <paramref name="filter"/>.
+        /// </summary>
+        /// <param name="filter">The filter.</param>
+        /// <returns>Entities which corespond to <paramref name="filter"/>.</returns>
+        public ICollection<TEntity> GetAll(Expression<Func<TEntity, bool>> filter)
+        {
+            var currentTable = this._dbContext.Set<TEntity>();
+            return currentTable.Where(filter).ToList();
+        }
+
+        /// <summary>
+        /// Adds or updates the specified <paramref name="model"/>.
+        /// </summary>
+        /// <param name="model">
+        /// The model to add or to update.
+        /// </param>
+        public void AddOrUpdate(TEntity model)
+        {
+            if (model == null)
+            {
+                throw new ArgumentNullException(nameof(model));
+            }
+
+            var currentTable = this._dbContext.Set<TEntity>();
+
+            if (model.Id > 0)
+            {
+                var originalTrack = currentTable.Find(model.Id);
+
+                // if the model exists in Db
+                if (originalTrack != null)
+                {
+                    var entry = this._dbContext.Entry(originalTrack);
+                    entry.CurrentValues.SetValues(model);
+                    entry.State = EntityState.Modified;
+                    return;
+                }
+            }
+            
+            // if it is a new model
+            currentTable.Add(model);
+        }
+
+        /// <summary>
+        /// Deletes a model with the specified <paramref name="id"/>.
+        /// </summary>
+        /// <param name="id">The model key.</param>
+        public void Delete(int id)
+        {
+            var currentTable = this._dbContext.Set<TEntity>();
+            var model = currentTable.Find(id);
+            if (model != null)
+            {
+                currentTable.Remove(model);
+            }
+        }
+
+        /// <summary>
+        /// Deletes the <paramref name="model"/> from the repository.
+        /// </summary>
+        /// <param name="model">
+        /// The model to remove.
+        /// </param>
+        public void Delete(TEntity model)
+        {
+            if (model == null)
+            {
+                throw new ArgumentNullException(nameof(model));
+            }
+
+            var currentTable = this._dbContext.Set<TEntity>();
+            currentTable.Remove(model);
+        }
+
+        #endregion //IRepository<TEntity> Members
+
+        #region IDisposable Pattern
+
+        /// <summary>
+        /// Disposes resources.
+        /// </summary>
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        /// <summary>
+        /// Disposes resources in case if <paramref name="disposing"/> is <b>true</b>
+        /// </summary>
+        /// <param name="disposing"></param>
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                this._dbContext?.Dispose();
+            }
+        }
+
+        #endregion //IDisposable Pattern
+    }
+}
