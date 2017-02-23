3a81459130c9df19c3c9de8aa3dceec9fec708ac
Litvinov Alexander <bumble-bee83@mail.ru>
2017-02-24 00:09:46 +0300

    + added model for Currency

---
+++ PVT.Q1.2017.Shop/Shop.Common/Models/Currency.cs
@@ -0,0 +1,26 @@
+﻿namespace Shop.Common.Models
+{
+    using Shop.Infrastructure.Models;
+
+    /// <summary>
+    /// Model for Currency
+    /// </summary>
+    public class Currency : BaseEntity
+    {
+        /// <summary>
+        /// ShortName of Currency
+        /// </summary>
+        public string ShortName { get; set; }
+
+        /// <summary>
+        /// FullName of Currency
+        /// </summary>
+        public string FullName { get; set; }
+
+        /// <summary>
+        /// Code of Currency
+        /// </summary>
+        public int Code { get; set; }
+
+    }
+}
