9c1ad322c8df1ea119edae99ac6f9bb6fb8ea372
forenheith <6537537@gmail.com>
2017-02-24 15:57:07 +0300

    + Track view model

---
+++ PVT.Q1.2017.Shop/PVT.Q1.2017.Shop/ViewModels/Track.cs
@@ -0,0 +1,27 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Track.cs" company="PVT.Q1.2017">
+//   PVT.Q1.2017
+// </copyright>
+// <summary>
+//   Defines the Track type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace PVT.Q1._2017.Shop.ViewModels
+{
+    using global::Shop.Infrastructure.Models;
+
+    /// <summary>
+    /// </summary>
+    public class Track
+    {
+        /// <summary>
+        /// </summary>
+        private ITrack _track;
+
+        public Track(TEntity track)
+        {
+            
+        }
+    }
+}
