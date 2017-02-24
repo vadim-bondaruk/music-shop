de268b0d22d95784e874aeb9bbb986a4794b9b13
Aliaksandr Kaptsiuh <Cheater-xg@yandex.ru>
2017-02-24 14:40:29 +0300

    + added BasePayment + added PaymentMethod (types) + added UserPaymentMethod (personal settings)

---
+++ PVT.Q1.2017.Shop/Shop.Common/Models/BasePayment.cs
@@ -0,0 +1,29 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="BasePayment.cs" company="PVT Q1 2017">
+//   All rights reserved
+// </copyright>
+// <summary>
+//   Defines the BasePayment type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Shop.Common.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Infrastructure.Models;
+
+    /// <summary>
+    /// Base model for every user payment method
+    /// </summary>
+    public class BasePayment : BaseEntity
+    {
+        /// <summary>
+        /// Description for payment method
+        /// </summary>
+        public string Description { get; set; }
+    }
+}
---
+++ PVT.Q1.2017.Shop/Shop.Common/Models/UserPaymentMethod.cs
@@ -0,0 +1,43 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="UserPaymentMethod.cs" company="PVT Q1 2017">
+//   All rights reserved
+// </copyright>
+// <summary>
+//   Defines the UserPaymentMethod type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Shop.Common.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Define settings for user payment methods
+    /// </summary>
+    public class UserPaymentMethod : BasePayment
+    {
+        /// <summary>
+        /// Gets or sets User for payment method settings
+        /// </summary>
+        public User User { get; set; }
+
+        /// <summary>
+        /// Gets or sets payment method
+        /// </summary>
+        public PaymentMethod PaymentMethod { get; set; }
+
+        /// <summary>
+        /// Gets or sets alias for this user payment method settings
+        /// </summary>
+        public string Alias { get; set; }
+
+        /// <summary>
+        /// Gets or sets default currency 
+        /// </summary>
+        public Currency Currency { get; set; }
+    }
+}
