6509bba33e48bbdda8af8a9599e8e06c8ee9d0fc
forenheith <6537537@gmail.com>
2017-02-24 16:40:48 +0300

    + Track.cshtml

--- PVT.Q1.2017.Shop/PVT.Q1.2017.Shop/ViewModels/Track.cs
+++ PVT.Q1.2017.Shop/PVT.Q1.2017.Shop/ViewModels/Track.cs
@@ -17,11 +17,92 @@ namespace PVT.Q1._2017.Shop.ViewModels
     {
         /// <summary>
         /// </summary>
-        private ITrack _track;
+        private readonly ITrack _track;
 
-        public Track(TEntity track)
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="Track" /> class.
+        /// </summary>
+        /// <param name="track">The track.</param>
+        public Track(ITrack track)
         {
-            
+            this._track = track;
+        }
+
+        /// <summary>
+        /// Gets the album.
+        /// </summary>
+        public string Album
+        {
+            get
+            {
+                return this._track.Album;
+            }
+        }
+
+        /// <summary>
+        /// Gets the artist.
+        /// </summary>
+        public string Artist
+        {
+            get
+            {
+                return this._track.Artist;
+            }
+        }
+
+        /// <summary>
+        /// Gets the genre.
+        /// </summary>
+        public string Genre
+        {
+            get
+            {
+                return this._track.Genre;
+            }
+        }
+
+        /// <summary>
+        /// Gets the id.
+        /// </summary>
+        public int Id
+        {
+            get
+            {
+                return this._track.Id;
+            }
+        }
+
+        /// <summary>
+        /// Gets the name.
+        /// </summary>
+        public string Name
+        {
+            get
+            {
+                return this._track.Name;
+            }
+        }
+
+        /// <summary>
+        /// Gets the release date.
+        /// </summary>
+        public string ReleaseDate
+        {
+            get
+            {
+                return this._track.ReleaseDate;
+            }
+        }
+
+        /// <summary>
+        /// Gets the year.
+        /// </summary>
+        public string Year
+        {
+            get
+            {
+                return this._track.Year;
+            }
         }
     }
 }
