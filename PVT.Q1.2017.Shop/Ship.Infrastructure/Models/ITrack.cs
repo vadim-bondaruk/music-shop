b85d11cd9faa5928337c1b57c4edfa00b33ae609
forenheith <6537537@gmail.com>
2017-02-24 16:40:58 +0300

    + ITrack

--- PVT.Q1.2017.Shop/Ship.Infrastructure/Models/ITrack.cs
+++ PVT.Q1.2017.Shop/Ship.Infrastructure/Models/ITrack.cs
@@ -13,5 +13,44 @@ namespace Shop.Infrastructure.Models
     /// </summary>
     public interface ITrack
     {
+        /// <summary>
+        ///     Gets or sets the album.
+        /// </summary>
+        string Album { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the artist.
+        /// </summary>
+        string Artist { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the duration.
+        /// </summary>
+        string Duration { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the genre.
+        /// </summary>
+        string Genre { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the id.
+        /// </summary>
+        int Id { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the name.
+        /// </summary>
+        string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets the release date.
+        /// </summary>
+        string ReleaseDate { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the year.
+        /// </summary>
+        string Year { get; set; }
     }
 }
