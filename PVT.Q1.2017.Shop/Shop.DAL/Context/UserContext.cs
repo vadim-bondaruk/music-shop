0d006343d899cfe78082f64cf6e9f2ccc6c52c76
Andrei Kukharenko <carbon_kas@mail.ru>
2017-02-24 15:58:42 +0300

    *little fix  +added UserContext.cs

---
+++ PVT.Q1.2017.Shop/Shop.DAL/Context/UserContext.cs
@@ -0,0 +1,23 @@
+﻿namespace Shop.DAL.Context
+{
+    using System;
+    using System.Data.Entity;
+    using Shop.DAL.Migrations;
+    using Shop.Common.Models;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public class UserContext : DbContext
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public UserContext() : base ("UserConnection")
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+
+    }
+}
