2fa4971100b07ebb812d19f31e32a91df1207fbe
forenheith <6537537@gmail.com>
2017-02-25 23:52:17 +0300

    * Downgrade ninject version

---
+++ PVT.Q1.2017.Shop/PVT.Q1.2017.Shop/Controllers/BaseController.cs
@@ -0,0 +1,30 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="BaseController.cs" company="PVT.Q1.2017">
+//   PVT.Q1.2017
+// </copyright>
+// <summary>
+//   Defines the BaseController type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace PVT.Q1._2017.Shop.Controllers
+{
+    using AutoMapper;
+
+    /// <summary>
+    /// </summary>
+    public abstract class BaseController
+    {
+        /// <summary>
+        /// </summary>
+        private IMapper _mapper;
+
+        /// <summary>
+        /// Gets the mapper.
+        /// </summary>
+        protected IMapper Mapper()
+        {
+            return null;
+        }
+    }
+}
