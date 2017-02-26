58e8e81be7d8f05adad68833f768c0ce7f1ca939
Andrei Kukharenko <carbon_kas@mail.ru>
2017-02-26 22:22:11 +0300

    *ListOfUsers replaced by UserInitializer

---
+++ PVT.Q1.2017.Shop/Shop.DAL/Data/UserInitializer.cs
@@ -0,0 +1,68 @@
+﻿namespace Shop.DAL.Data
+{
+    using System.Collections.Generic;
+    using System.Data.Entity;
+    using Shop.Common.Models;
+    using Shop.DAL.Context;
+    using Shop.Infrastructure.Enums;
+
+    /// <summary>
+    /// Adds data to the context for seeding. A storage for entities of users
+    /// </summary>
+    class UserInitializer : DropCreateDatabaseIfModelChanges<UserContext>
+    {
+        protected override void Seed(UserContext context)
+        {
+            var _users = new List<User>
+            {
+                new User
+                {
+                FirstName = "John",
+                LastName = "Gates",
+                Login = "kasper",
+                Sex = 'M',
+                UserRole = UserRoles.User,
+                Email = "KasperKasper@gmail.com",
+                Password = "СлаваКПСС!11"
+                },
+
+                new User
+                {
+                FirstName = "Alice",
+                LastName = "McNeal",
+                Login = "alisochka",
+                Sex = 'W',
+                UserRole = UserRoles.User,
+                Email = "alice1@gmail.com",
+                Password = "100VK500"
+                },
+
+                new User
+                {
+                FirstName = "Artur",
+                LastName = "Li",
+                Login = "arturio",
+                Sex = 'M',
+                UserRole = UserRoles.Admin,
+                Email = "artII@mail.ru",
+                Password = "nobodyKnowMyP-d1"
+                },
+
+                new User
+                {
+                FirstName = "Abziz",
+                LastName = "Anand",
+                Login = "OilMagnat",
+                Sex = 'M',
+                UserRole = UserRoles.VIPUser,
+                Email = "blablabla@gmail.com",
+                Password = "12345"
+                }
+            };
+
+            _users.ForEach(s => context.Users.Add(s));
+            context.SaveChanges();
+            
+        }
+    }
+}
