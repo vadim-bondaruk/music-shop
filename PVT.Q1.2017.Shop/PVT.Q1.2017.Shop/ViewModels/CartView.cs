1fcbbf952e6f4b0b8759eab20b052feadd8c5753
Sergey Ishmuratov <uzer@xakep.ru>
2017-02-26 19:09:36 +0300

    + added CartView model

---
+++ PVT.Q1.2017.Shop/PVT.Q1.2017.Shop/ViewModels/CartView.cs
@@ -0,0 +1,17 @@
+﻿namespace PVT.Q1._2017.Shop.ViewModels
+{
+    using global::Shop.Common.Models;
+    using System.Collections.Generic;
+
+    public class CartView
+    {
+        /// <summary>
+        /// Gets or sets a Collection of tracks for View
+        /// </summary>
+        public IList<Track> Tracks { get; set; }
+        /// <summary>
+        /// Get total price of cart
+        /// </summary>
+        public decimal TotalPrice { get; }
+    }
+}
