3114ac6d592b6f62d54cb775dc20fdc3589f3f2e
Olga Ananich <cachetonaru@yandex.ru>
2017-02-27 14:58:11 +0300

    * CartController

--- PVT.Q1.2017.Shop/PVT.Q1.2017.Shop/Controllers/CartController.cs
+++ PVT.Q1.2017.Shop/PVT.Q1.2017.Shop/Controllers/CartController.cs
@@ -1,9 +1,11 @@
 ﻿namespace PVT.Q1._2017.Shop.Controllers.Cart
 {
+    using System.Collections.Generic;
     using System.Web.Mvc;
     using global::Shop.Common.Models;
     using global::Shop.DAL.Repositories;
     using global::Shop.Infrastructure.Repositories;
+    using ViewModels;
 
     /// <summary>
     /// Контоллер для корзины покупателя
@@ -40,8 +42,17 @@
         [HttpGet]
         public ActionResult Index(int currentUserId, string returnUrl)
         {
-            // создание ViewModel
-            return this.View();
+            var cardView = new CartView { Tracks = new List<Track>() };
+            var card = this._cartRepository.GetAll(c => c.User.Id.Equals(currentUserId));
+            foreach (var c in card)
+            {
+                foreach (var t in c.Tracks)
+                {
+                    cardView.Tracks.Add(t);
+                }
+            }
+
+            return this.View(cardView);
         }
 
         /// <summary>
@@ -56,7 +67,7 @@
         /// <param name="returnUrl">
         /// Путь для возврата к покупкам
         /// </param>
-        public RedirectToRouteResult AddToCart(int currentUserId, int trackID, string returnUrl)
+        public RedirectToRouteResult AddTrackToCart(int currentUserId, int trackID, string returnUrl)
         {
             // добавление в репозиторий
             return this.RedirectToRoute(new { controller = "CartController", action = "Index", currentUserId = currentUserId, returnUrl = returnUrl });
@@ -74,7 +85,7 @@
         /// <param name="returnUrl">
         /// Путь для возврата к покупкам
         /// </param>
-        public RedirectToRouteResult DeleteFromCart(int currentUserId, int cartId, string returnUrl)
+        public RedirectToRouteResult DeleteTrackFromCart(int currentUserId, int cartId, string returnUrl)
         {
             this._cartRepository.Delete(cartId);
             return this.RedirectToRoute(new { controller = "CartController", action = "Index", currentUserId = currentUserId, returnUrl = returnUrl });
