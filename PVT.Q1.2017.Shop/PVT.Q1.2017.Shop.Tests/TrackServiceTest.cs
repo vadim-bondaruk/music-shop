548eee62f542be32aa7541793ba99c10ee907e92
Aliaksei Padabed <Aliaksei.Padabed@gmail.com>
2017-03-01 19:44:54 +0300

    * added some tests for track service

--- PVT.Q1.2017.Shop/PVT.Q1.2017.Shop.Tests/TrackServiceTest.cs
+++ PVT.Q1.2017.Shop/PVT.Q1.2017.Shop.Tests/TrackServiceTest.cs
@@ -1,9 +1,13 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Ninject;
 using Shop.BLL;
+using Shop.BLL.Exceptions;
 using Shop.BLL.Services;
 using Shop.Common.Models;
+using Shop.DAL;
+using Shop.Infrastructure.Repositories;
 using Shop.Infrastructure.Services;
 
 namespace PVT.Q1._2017.Shop.Tests
@@ -20,54 +24,59 @@ namespace PVT.Q1._2017.Shop.Tests
 
         #endregion //Fields
 
+        #region Constructors
+
         public TrackServiceTest()
         {
             this._kernel = new StandardKernel(new DefaultServicesNinjectModule());
         }
 
-        private TestContext testContextInstance;
-
-        /// <summary>
-        ///Gets or sets the test context which provides
-        ///information about and functionality for the current test run.
-        ///</summary>
-        public TestContext TestContext
-        {
-            get
-            {
-                return testContextInstance;
-            }
-            set
-            {
-                testContextInstance = value;
-            }
-        }
-
-        #region Additional test attributes
-        //
-        // You can use the following additional attributes as you write your tests:
-        //
-        // Use ClassInitialize to run code before running the first test in the class
-        // [ClassInitialize()]
-        // public static void MyClassInitialize(TestContext testContext) { }
-        //
-        // Use ClassCleanup to run code after all tests in a class have run
-        // [ClassCleanup()]
-        // public static void MyClassCleanup() { }
-        //
-        // Use TestInitialize to run code before running each test 
-        // [TestInitialize()]
-        // public void MyTestInitialize() { }
-        //
-        // Use TestCleanup to run code after each test has run
-        // [TestCleanup()]
-        // public void MyTestCleanup() { }
-        //
-        #endregion
+        #endregion //Constructors
 
         [TestMethod]
-        public void RegisterTrackTest()
+        public void RegisterValidTrackTest()
         {
+            var artist = new Artist { Name = "Adele" };
+
+            var repositoryFactory = this._kernel.Get<IRepositoryFactory>();
+            using (var repository = repositoryFactory.CreateRepository<Artist>())
+            {
+                repository.AddOrUpdate(artist);
+                repository.SaveChanges();
+
+                Assert.IsTrue(repository.GetAll(a => a.Name == artist.Name).Any());
+            }
+
+            var album = new Album { Name = "Some Single" };
+            using (var repository = repositoryFactory.CreateRepository<Album>())
+            {
+                repository.AddOrUpdate(album);
+                repository.SaveChanges();
+
+                Assert.IsTrue(repository.GetAll(a => a.Name == album.Name).Any());
+            }
+
+            var currency = new Currency { Code = 840, ShortName = "$", FullName = "USD" };
+            using (var repository = repositoryFactory.CreateRepository<Currency>())
+            {
+                repository.AddOrUpdate(currency);
+                repository.SaveChanges();
+
+                Assert.IsTrue(repository.GetAll(c => c.Code == currency.Code).Any());
+            }
+
+            var track = new Track { Name = "Hello", Album = album, Artist = artist };
+
+            var trackService = this._kernel.Get<IService<Track>>();
+            trackService.Register(track);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidEntityException))]
+        public void RegisterInvalidTrackTest()
+        {
+            var trackService = this._kernel.Get<IService<Track>>();
+            trackService.Register(new Track());
         }
     }
 }
