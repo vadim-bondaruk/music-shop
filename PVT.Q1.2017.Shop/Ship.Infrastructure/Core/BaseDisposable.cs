bc2eb4a2df8d32421fee693aedcadbfb057e044f
Vadim Ilyin <ivp.net@outlook.com>
2017-03-02 11:02:30 +0300

    + added basedisposable class

---
+++ PVT.Q1.2017.Shop/Ship.Infrastructure/Core/BaseDisposable.cs
@@ -0,0 +1,26 @@
+﻿namespace Shop.Infrastructure.Core
+{
+    using System;
+    using System.Diagnostics;
+
+    public abstract class BaseDisposable : IDisposable
+    {
+        protected bool Disposed = false;
+
+        ~BaseDisposable()
+        {
+            Dispose(false);
+#if DEBUG
+            Debug.WriteLine("GC Deleted: <" + this.GetType().Name + ">");
+#endif
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);//don't call finalizator
+        }
+
+        protected abstract void Dispose(bool disposing);
+    }
+}
