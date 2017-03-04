adafd3aa523ef22eebede864db78bb77b3f93758
Aliaksei Padabed <Aliaksei.Padabed@gmail.com>
2017-03-04 14:20:12 +0300

    + added IAbumService and IArtistService

---
+++ PVT.Q1.2017.Shop/Shop.BLL/Services/Infrastructure/IAlbumService.cs
@@ -0,0 +1,20 @@
+﻿namespace Shop.BLL.Services.Infrastructure
+{
+    using Common.Models;
+    using Shop.Infrastructure.Services;
+
+    /// <summary>
+    /// The album service.
+    /// </summary>
+    public interface IAlbumService : IService<Album>
+    {
+        /// <summary>
+        /// Returns the album with the specified <paramref name="id"/>.
+        /// </summary>
+        /// <param name="id">The album id.</param>
+        /// <returns>
+        /// The album with the specified <paramref name="id"/> or <b>null</b> if album doesn't exist.
+        /// </returns>
+        Album GetAlbumInfo(int id);
+    }
+}
---
+++ PVT.Q1.2017.Shop/Shop.BLL/Services/Infrastructure/IArtistService.cs
@@ -0,0 +1,20 @@
+﻿namespace Shop.BLL.Services.Infrastructure
+{
+    using Common.Models;
+    using Shop.Infrastructure.Services;
+
+    /// <summary>
+    /// The artist service.
+    /// </summary>
+    public interface IArtistService : IService<Artist>
+    {
+        /// <summary>
+        /// Returns the artist with the specified <paramref name="id"/>.
+        /// </summary>
+        /// <param name="id">The artist id.</param>
+        /// <returns>
+        /// The artist with the specified <paramref name="id"/> or <b>null</b> if artist doesn't exist.
+        /// </returns>
+        Artist GetArtistInfo(int id);
+    }
+}
