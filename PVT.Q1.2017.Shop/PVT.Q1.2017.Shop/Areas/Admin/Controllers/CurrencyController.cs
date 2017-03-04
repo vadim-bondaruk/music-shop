9786d5d0326fa4fdfecb0c87df9a3fce165508e4
Slava Peshko <slava.peshko@gmail.com>
2017-03-05 01:19:58 +0300

    *bug fix

--- PVT.Q1.2017.Shop/PVT.Q1.2017.Shop/Areas/Admin/Controllers/CurrencyController.cs
+++ PVT.Q1.2017.Shop/PVT.Q1.2017.Shop/Areas/Admin/Controllers/CurrencyController.cs
@@ -1,17 +1,12 @@
 ﻿namespace PVT.Q1._2017.Shop.Areas.Admin.Controllers
 {
     using System.Web.Mvc;
-    using System.Linq;
-    using System.Data.Entity;
-    using System.Data;
     using global::Shop.Common.Models;
-    using global::Shop.DAL.Repositories.Infrastruture;
-    using global::Shop.Infrastructure;
-    using global::Shop.Infrastructure.Services;
     using global::Shop.Infrastructure.Repositories;
-    using global::Shop.DAL.Context;
-    using Shop.ViewModels;
 
+    /// <summary>
+    /// 
+    /// </summary>
     [Authorize]
     public class CurrencyController : Controller
     {
@@ -32,7 +27,7 @@
         /// 
         /// </summary>
         /// <param name="repo"></param>
-        public CurrencyController(IRepository<Currency> currencyRepo, IRepository<CurrencyRate> currencyRateRepo )
+        public CurrencyController(IRepository<Currency> currencyRepo, IRepository<CurrencyRate> currencyRateRepo)
         {
             this._currencyRepository = currencyRepo;
             this._currencyRateRepository = currencyRateRepo;
@@ -46,45 +41,17 @@
         [HttpGet]
         public ActionResult Index()
         {
-            //var currencyis = _currencyRepository.GetAll(x=> x.CurrencyRates);
-
-            var curViewMOdel = currencyis.Select(x => new CurrencyViewModel()
-            {
-                FullName = x.FullName,
-                ShortName = x.ShortName,
-                CrossCurce = x.CurrencyRates.Select(y => new CurrencyViewModel.CrossCurrencyViewModel() { }).ToList()
-            });
-
-            //CurrencyViewModel cur = new CurrencyViewModel();
-            //cur.FullName 
-
-
-            // cyrrency and cyrrencyRate
-            //var currency = DbContext.Currencies.Include(x => x.CurrencyRates);
-            return View(curViewMOdel);
+            return this.View();
         }
 
         /// <summary>
         /// 
         /// </summary>
-        /// <param name="currencyId"></param>
+        /// <param name="id"></param>
         /// <returns></returns>
         public ActionResult Edit(int id)
         {
-            //    var currencyRate = DbContext.CurrencyRates
-            //        .Where(x => x.CurrencyId == id)
-            //        .FirstOrDefault();
-
-            //    if(currencyRate == null)
-            //    {
-            //        return HttpNotFound();
-            //    }
-
-            //    ViewBag.CurrencyId = new SelectList(DbContext.Currencies, "Id", "Name", currencyRate.CurrencyId);
-            //    ViewBag.CurrencyRateId = new SelectList(DbContext.CurrencyRates, "Id", "Name", currencyRate.Id);
-
-            //    return this.View(currencyRate);
-            return View();
+            return this.View();
         }
 
         /// <summary>
@@ -95,21 +62,16 @@
         [HttpPost]
         public ActionResult Edit(CurrencyRate currencyRate)
         {
-            //if (ModelState.IsValid)
-            //{
-            //    _repository.AddOrUpdate(currencyRate);
-            //    return RedirectToAction("Index");
-            //}
-
-            //return View(currencyRate);
+            return this.View();
         }
 
-
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
         public ActionResult Delete()
         {
-            return View();
+            return this.View();
         }
-
-
     }
 }
--- PVT.Q1.2017.Shop/PVT.Q1.2017.Shop/ViewModels/CurrencyViewModel.cs
+++ PVT.Q1.2017.Shop/PVT.Q1.2017.Shop/ViewModels/CurrencyViewModel.cs
@@ -1,20 +1,45 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
-namespace PVT.Q1._2017.Shop.ViewModels
+﻿namespace PVT.Q1._2017.Shop.ViewModels
 {
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// 
+    /// </summary>
     public class CurrencyViewModel
     {
+        /// <summary>
+        /// 
+        /// </summary>
         public string FullName { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
         public string ShortName { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
         public List<CrossCurrencyViewModel> CrossCurce { get; set; }
 
+        /// <summary>
+        /// 
+        /// </summary>
         public class CrossCurrencyViewModel
         {
+            /// <summary>
+            /// 
+            /// </summary>
             public string FullName { get; set; }
+
+            /// <summary>
+            /// 
+            /// </summary>
             public string ShortName { get; set; }
+
+            /// <summary>
+            /// 
+            /// </summary>
             public decimal CrossCourse { get; set; }
         }
     }
