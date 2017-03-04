19e62f3da512d2a984c05a7c1cf921ea17d3ee50
Aliaksei Padabed <Aliaksei.Padabed@gmail.com>
2017-03-04 14:21:06 +0300

    * improved TrackValidator

--- PVT.Q1.2017.Shop/Shop.BLL/Validators/TrackValidator.cs
+++ PVT.Q1.2017.Shop/Shop.BLL/Validators/TrackValidator.cs
@@ -3,12 +3,55 @@
     using System;
     using Common.Models;
     using Exceptions;
+    using Services.Infrastructure;
 
     /// <summary>
     /// The track validator.
     /// </summary>
     public class TrackValidator : NamedEntityValidator<Track>
     {
+        #region Fields
+
+        /// <summary>
+        /// The album service.
+        /// </summary>
+        private readonly IAlbumService _albumService;
+
+        /// <summary>
+        /// The artist service.
+        /// </summary>
+        private readonly IArtistService _artistService;
+
+        #endregion //Fields
+
+        #region Constructors
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TrackValidator"/> class.
+        /// </summary>
+        /// <param name="albumService">The album service.</param>
+        /// <param name="artistService">The artist service</param>
+        /// <exception cref="ArgumentNullException">
+        /// When the <paramref name="albumService"/> is null or the <paramref name="artistService"/> is null.
+        /// </exception>
+        public TrackValidator(IAlbumService albumService, IArtistService artistService)
+        {
+            if (albumService == null)
+            {
+                throw new ArgumentNullException(nameof(albumService));
+            }
+
+            if (artistService == null)
+            {
+                throw new ArgumentNullException(nameof(artistService));
+            }
+
+            this._albumService = albumService;
+            this._artistService = artistService;
+        }
+
+        #endregion //Constructors
+
         #region Public Methods
 
         /// <summary>
@@ -25,15 +68,7 @@
         /// </exception>
         public override void Validate(Track track)
         {
-            if (track == null)
-            {
-                throw new ArgumentNullException(nameof(track));
-            }
-
-            if (!TrackValidator.IsNameValid(track.Name))
-            {
-                throw new InvalidEntityException("Invalid track name specified.");
-            }
+            base.Validate(track);
 
             if (!track.ArtistId.HasValue)
             {
@@ -43,6 +78,22 @@
             if (!track.AlbumId.HasValue)
             {
                 throw new InvalidEntityException("Invalid track album specified.");
+            }
+
+            if (!this._albumService.IsRegistered(track.AlbumId.Value))
+            {
+                throw new EntityNotFoundException($"Album with id='{ track.AlbumId }' doesn't exist.");
+            }
+
+            if (!this._artistService.IsRegistered(track.ArtistId.Value))
+            {
+                throw new EntityNotFoundException($"Artist with id='{ track.ArtistId }' doesn't exist.");
+            }
+
+            var album = this._albumService.GetAlbumInfo(track.AlbumId.Value);
+            if (album.ArtistId != track.ArtistId)
+            {
+                throw new InvalidEntityException($"The track and album with id='{ track.AlbumId }' has different artists.");
             }
         }
 
@@ -68,6 +119,22 @@
             }
 
             if (!track.AlbumId.HasValue)
+            {
+                return false;
+            }
+
+            if (!this._albumService.IsRegistered(track.AlbumId.Value))
+            {
+                return false;
+            }
+
+            if (!this._artistService.IsRegistered(track.ArtistId.Value))
+            {
+                return false;
+            }
+
+            var album = this._albumService.GetAlbumInfo(track.AlbumId.Value);
+            if (album.ArtistId != track.ArtistId)
             {
                 return false;
             }
