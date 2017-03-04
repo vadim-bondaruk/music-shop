2eb09a09e08541b5fa90e264affa46ed1a919a8d
Slava Peshko <slava.peshko@gmail.com>
2017-03-05 00:29:38 +0300

    + added CurrencyController, CurrencyViewModel, CurrencyService

---
+++ PVT.Q1.2017.Shop/PVT.Q1.2017.Shop/ViewModels/CurrencyViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PVT.Q1._2017.Shop.ViewModels
+{
+    public class CurrencyViewModel
+    {
+        public string FullName { get; set; }
+        public string ShortName { get; set; }
+        public List<CrossCurrencyViewModel> CrossCurce { get; set; }
+
+        public class CrossCurrencyViewModel {
+            public string FullName { get; set; }
+            public string ShortName { get; set; }
+            public decimal 
+        }
+    }
+}
