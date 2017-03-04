6096f36de3f7cdb4eb4725b15fbefdf13b60d391
Aliaksei Padabed <Aliaksei.Padabed@gmail.com>
2017-03-04 19:39:56 +0300

    * improved feedback and vote validators

--- PVT.Q1.2017.Shop/Shop.BLL/Validators/FeedbackValidator.cs
+++ PVT.Q1.2017.Shop/Shop.BLL/Validators/FeedbackValidator.cs
@@ -3,12 +3,60 @@
     using System;
     using Common.Models;
     using Exceptions;
+    using Helpers;
     using Infrastructure.Validators;
+    using Services.Infrastructure;
 
     /// <summary>
+    /// The feedback validator.
     /// </summary>
     public class FeedbackValidator : IValidator<Feedback>
     {
+        #region Fields
+
+        /// <summary>
+        /// The track service.
+        /// </summary>
+        private readonly ITrackService _trackService;
+
+        /// <summary>
+        /// The user service.
+        /// </summary>
+        private readonly IUserService _userService;
+
+        #endregion //Fields
+
+        #region Constructors
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FeedbackValidator"/> class.
+        /// </summary>
+        /// <param name="trackService">
+        /// The track service.
+        /// </param>
+        /// <param name="userService">
+        /// The user service.
+        /// </param>
+        public FeedbackValidator(ITrackService trackService, IUserService userService)
+        {
+            if (trackService == null)
+            {
+                throw new ArgumentNullException(nameof(trackService));
+            }
+
+            if (userService == null)
+            {
+                throw new ArgumentNullException(nameof(userService));
+            }
+
+            this._trackService = trackService;
+            this._userService = userService;
+        }
+
+        #endregion //Constructors
+
+        #region Public Static Methods
+
         /// <summary>
         /// Determines whether the <paramref name="comment"/> is valid.
         /// </summary>
@@ -23,14 +71,28 @@
             return !string.IsNullOrWhiteSpace(comment);
         }
 
+        #endregion //Public Static Methods
+
+        #region IValidator<Feedback> Members
+
         /// <summary>
         /// Validates the specified <paramref name="feedback"/>.
         /// </summary>
         /// <param name="feedback">
-        /// The item to validate.
+        /// The feedback to validate.
         /// </param>
+        /// <exception cref="ArgumentNullException">
+        /// When the <paramref name="feedback"/> is null.
+        /// </exception>
+        /// <exception cref="InvalidEntityException">
+        /// When the <paramref name="feedback"/> comment is invalid.
+        /// </exception>
+        /// <exception cref="EntityNotFoundException">
+        /// When the track or user doesn't exist.
+        /// </exception>
         public virtual void Validate(Feedback feedback)
         {
+            ValidatorHelper.CheckFeedbackForNull(feedback);
             if (feedback == null)
             {
                 throw new ArgumentNullException(nameof(feedback));
@@ -39,6 +101,16 @@
             if (!IsCommentValid(feedback.Comments))
             {
                 throw new InvalidEntityException("Invalid comment specified.");
+            }
+
+            if (!this._trackService.IsRegistered(feedback.TrackId))
+            {
+                throw new EntityNotFoundException($"Track with id='{ feedback.TrackId }' doesn't exist.");
+            }
+
+            if (!this._userService.IsRegistered(feedback.UserId))
+            {
+                throw new EntityNotFoundException($"User with id='{ feedback.UserId }' doesn't exist.");
             }
         }
 
@@ -63,7 +135,19 @@
                 return false;
             }
 
+            if (!this._trackService.IsRegistered(feedback.TrackId))
+            {
+                return false;
+            }
+
+            if (!this._userService.IsRegistered(feedback.UserId))
+            {
+                return false;
+            }
+
             return true;
         }
+
+        #endregion //IValidator<Feedback> Members
     }
 }
--- PVT.Q1.2017.Shop/Shop.BLL/Validators/VoteValidator.cs
+++ PVT.Q1.2017.Shop/Shop.BLL/Validators/VoteValidator.cs
@@ -4,11 +4,58 @@
     using Common.Models;
     using Exceptions;
     using Infrastructure.Validators;
+    using Services.Infrastructure;
 
     /// <summary>
+    /// The vote validator.
     /// </summary>
     public class VoteValidator : IValidator<Vote>
     {
+        #region Fields
+
+        /// <summary>
+        /// The track service.
+        /// </summary>
+        private readonly ITrackService _trackService;
+
+        /// <summary>
+        /// The user service.
+        /// </summary>
+        private readonly IUserService _userService;
+
+        #endregion //Fields
+
+        #region Constructors
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FeedbackValidator"/> class.
+        /// </summary>
+        /// <param name="trackService">
+        /// The track service.
+        /// </param>
+        /// <param name="userService">
+        /// The user service.
+        /// </param>
+        public VoteValidator(ITrackService trackService, IUserService userService)
+        {
+            if (trackService == null)
+            {
+                throw new ArgumentNullException(nameof(trackService));
+            }
+
+            if (userService == null)
+            {
+                throw new ArgumentNullException(nameof(userService));
+            }
+
+            this._trackService = trackService;
+            this._userService = userService;
+        }
+
+        #endregion //Constructors
+
+        #region Public Static Methods
+
         /// <summary>
         /// Determines whether the <paramref name="mark"/> is valid.
         /// </summary>
@@ -23,12 +70,25 @@
             return Enum.IsDefined(typeof(Mark), mark);
         }
 
+        #endregion //Public Static Methods
+
+        #region IValidator<Vote> Members
+
         /// <summary>
         /// Validates the specified <paramref name="vote"/>.
         /// </summary>
         /// <param name="vote">
-        /// The vote.
+        /// The vote to validate.
         /// </param>
+        /// <exception cref="ArgumentNullException">
+        /// When the <paramref name="vote"/> is null.
+        /// </exception>
+        /// <exception cref="InvalidEntityException">
+        /// When the <paramref name="vote"/> mark is invalid.
+        /// </exception>
+        /// <exception cref="EntityNotFoundException">
+        /// When the track or user doesn't exist.
+        /// </exception>
         public virtual void Validate(Vote vote)
         {
             if (vote == null)
@@ -38,7 +98,17 @@
 
             if (!IsMarkValid(vote.Mark))
             {
-                throw new InvalidEntityException("Invalid comment specified.");
+                throw new InvalidEntityException("Invalid mark specified.");
+            }
+
+            if (!this._trackService.IsRegistered(vote.TrackId))
+            {
+                throw new EntityNotFoundException($"Track with id='{ vote.TrackId }' doesn't exist.");
+            }
+
+            if (!this._userService.IsRegistered(vote.UserId))
+            {
+                throw new EntityNotFoundException($"User with id='{ vote.UserId }' doesn't exist.");
             }
         }
 
@@ -46,7 +116,7 @@
         /// Determines whether the <paramref name="vote"/> is valid.
         /// </summary>
         /// <param name="vote">
-        /// The feedback to verify.
+        /// The vote to verify.
         /// </param>
         /// <returns>
         /// <b>true</b> if the <paramref name="vote"/> is valid; otherwise <b>false</b>.
@@ -63,7 +133,19 @@
                 return false;
             }
 
+            if (!this._trackService.IsRegistered(vote.TrackId))
+            {
+                return false;
+            }
+
+            if (!this._userService.IsRegistered(vote.UserId))
+            {
+                return false;
+            }
+
             return true;
         }
+
+        #endregion //IValidator<Vote> Members
     }
 }
