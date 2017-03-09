248bc011a546b56989a473deb6e9f8dc40608e16
Vadim Ilyin <ivp.net@outlook.com>
2017-03-10 02:42:59 +0300

    * edted ICurrencyRateService

--- PVT.Q1.2017.Shop/Shop.BLL/Services/Infrastructure/ICurrencyRateService.cs
+++ PVT.Q1.2017.Shop/Shop.BLL/Services/Infrastructure/ICurrencyRateService.cs
@@ -1,13 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Shop.BLL.Services.Infrastructure
+﻿namespace Shop.BLL.Services.Infrastructure
 {
+    using System;
+    using System.Collections.Generic;
+    using Common.Models;
+    
+    /// <summary>
+    /// Currency service interface
+    /// </summary>
     public interface ICurrencyRateService
     {
-        
+        /// <summary>
+        /// Get rate of currency by date
+        /// </summary>
+        /// <param name="from">Currency</param>
+        /// <param name="to">Target currency</param>
+        /// <param name="date">Date</param>
+        /// <returns></returns>
+        decimal GetRateByDate(int from, int to, DateTime date);
+
+        /// <summary>
+        /// Get rates by date
+        /// </summary>
+        /// <param name="date">Date</param>
+        /// <returns><see cref="IEnumerable{CurrencyRate}"/></returns>
+        IEnumerable<CurrencyRate> GetActualRates(DateTime? date = null);
     }
 }
