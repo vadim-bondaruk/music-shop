2a08c1eb83ad8fa5bfb0f456e605086df91ffa3d
Vadim Ilyin <ivp.net@outlook.com>
2017-03-09 17:06:31 +0300

    + add Currency rate service

---
+++ PVT.Q1.2017.Shop/Shop.BLL/Services/CurrencyRateService.cs
@@ -0,0 +1,13 @@
+﻿using Shop.BLL.Services.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.BLL.Services
+{
+    public class CurrencyRateService : ICurrencyRateService
+    {
+    }
+}
