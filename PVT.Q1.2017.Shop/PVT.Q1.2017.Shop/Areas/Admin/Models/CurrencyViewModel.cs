8ad24cc5e01ed237fdd4a6a8ef57070890568f8b
Slava Peshko <slava.peshko@gmail.com>
2017-03-09 15:55:51 +0300

    +added for admin models CurrencyViewModel

---
+++ PVT.Q1.2017.Shop/PVT.Q1.2017.Shop/Areas/Admin/Models/CurrencyViewModel.cs
@@ -0,0 +1,37 @@
+﻿namespace PVT.Q1._2017.Shop.Areas.Admin.Models
+{
+    /// <summary>
+    /// </summary>
+    public class CurrencyViewModel
+    {
+        /// <summary>
+        /// Gets or sets the identifier
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Soft deleted flag
+        /// </summary>
+        public bool IsDeleted { get; set; }
+
+        /// <summary>
+        /// Gets or sets the shortname
+        /// </summary>
+        public string ShortName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the fullname
+        /// </summary>
+        public string FullName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the code
+        /// </summary>
+        public int Code { get; set; }
+
+        /// <summary>
+        /// Gets or sets the graphic currency symbol.
+        /// </summary>
+        public string Symbol { get; set; }
+    }
+}
