d1172d4ad712084b16511b3e03d1ec1eb337948e
forenheith <forenheith@gmail.com>
2017-03-11 00:20:13 +0300

    * renew TrackViewModel

--- PVT.Q1.2017.Shop/PVT.Q1.2017.Shop/ViewModels/TrackViewModel.cs
+++ PVT.Q1.2017.Shop/PVT.Q1.2017.Shop/ViewModels/TrackViewModel.cs
@@ -1,53 +1,54 @@
 ﻿namespace PVT.Q1._2017.Shop.ViewModels
 {
-    #region using
-
+    using System;
+    using System.Collections.Generic;
     using global::Shop.Common.Models;
-    using global::Shop.DAL.Context;
     using global::Shop.DAL.Repositories;
-
-    #endregion
+    using Ninject;
 
     /// <summary>
-    /// 
     /// </summary>
     public class TrackViewModel
     {
         /// <summary>
-        /// 
         /// </summary>
         private readonly TrackViewModel _track;
 
         /// <summary>
-        /// 
         /// </summary>
         private BaseRepository<Track> _repo;
 
         /// <summary>
-        /// <para>
-        /// Initializes a new instance of the <see cref="TrackViewModel" />
-        /// </para>
-        /// <para>class.</para>
+        ///     <para>
+        ///         Initializes a new instance of the <see cref="TrackViewModel" />
+        ///     </para>
+        ///     <para>class.</para>
         /// </summary>
         /// <param name="track">The track.</param>
         public TrackViewModel()
         {
-            this._repo = new TrackBaseRepository(new ShopContext());
+            var kernel = new StandardKernel();
+            this._repo = kernel.Get<BaseRepository<Track>>();
         }
 
         /// <summary>
-        /// Gets or sets the album.
+        ///     Gets or sets the album.
         /// </summary>
-        public Album Album { get; set; }
+        public ICollection<Album> Album { get; set; }
 
         /// <summary>
-        /// Gets or sets the artist.
+        ///     Gets or sets the artist.
         /// </summary>
         public Artist Artist { get; set; }
 
         /// <summary>
-        /// Gets or sets the track.
+        ///     Gets or sets the duration.
         /// </summary>
-        public Track Track { get; set; }
+        public TimeSpan Duration { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the track.
+        /// </summary>
+        public string Name { get; set; }
     }
 }
