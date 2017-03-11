de994c30548da86b5d85b87992f6cef4f87dd4ee
Aliaksandr Kaptsiuh <Cheater-xg@yandex.ru>
2017-03-11 22:31:50 +0300

    + added UserPaymentMethodService * update Ninject config

--- PVT.Q1.2017.Shop/Shop.BLL/DefaultServicesNinjectModule.cs
+++ PVT.Q1.2017.Shop/Shop.BLL/DefaultServicesNinjectModule.cs
@@ -41,6 +41,8 @@
 
             Bind<IUserService>().To<UserService>();
 
+            Bind<IUserPaymentMethodService>().To<UserPaymentMethodService>();
+
             Bind<ICurrencyService>().To<CurrencyService>();
         }
     }
--- PVT.Q1.2017.Shop/Shop.BLL/Services/UserPaymentMethodService.cs
+++ PVT.Q1.2017.Shop/Shop.BLL/Services/UserPaymentMethodService.cs
@@ -1,12 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Shop.BLL.Services
+﻿namespace Shop.BLL.Services
 {
-    class UserPaymentMethodService
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Common.Models;
+    using DAL.Infrastruture;
+    using Infrastructure;
+
+    public class UserPaymentMethodService : BaseService, IUserPaymentMethodService
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="UserPaymentMethodService"/> class.
+        /// </summary>
+        /// <param name="factory">
+        /// The repository factory.
+        /// </param>
+        public UserPaymentMethodService(IRepositoryFactory factory) : base(factory)
+        {
+        }
+
+        public ICollection<UserPaymentMethod> UserPaymentMethodsByUser(User user)
+        {
+            if (user != null)
+            {
+                using (var repository = this.Factory.GetUserPaymentMethodRepository())
+                {
+                    return repository.GetAll(a => a.User.Equals(user));
+                }
+            }
+            return null;
+        }
     }
 }
