71eb3aef469be79b46cad075077d6b4f89f6e42b
Aliaksei Padabed <Aliaksei.Padabed@gmail.com>
2017-03-12 21:54:05 +0300

    * fixed StyleCop compilation error * fixed RepositoryFactoryMoq compilation error

--- PVT.Q1.2017.Shop/Shop.BLL/Services/UserPaymentMethodService.cs
+++ PVT.Q1.2017.Shop/Shop.BLL/Services/UserPaymentMethodService.cs
@@ -1,14 +1,13 @@
 ﻿namespace Shop.BLL.Services
 {
-    using System;
     using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
     using Common.Models;
     using DAL.Infrastruture;
     using Infrastructure;
 
+    /// <summary>
+    /// The user payment method service
+    /// </summary>
     public class UserPaymentMethodService : BaseService, IUserPaymentMethodService
     {
         /// <summary>
@@ -21,6 +20,15 @@
         {
         }
 
+        /// <summary>
+        /// Returns all payment methods available for the specified <paramref name="user"/>.
+        /// </summary>
+        /// <param name="user">
+        /// The user.
+        /// </param>
+        /// <returns>
+        ///  All payment methods available for the specified <paramref name="user"/>.
+        /// </returns>
         public ICollection<UserPaymentMethod> UserPaymentMethodsByUser(User user)
         {
             if (user != null)
@@ -30,6 +38,7 @@
                     return repository.GetAll(a => a.User.Equals(user));
                 }
             }
+
             return null;
         }
     }
