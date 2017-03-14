67933fa91fc32e62534a24019f39a9c40a07b12e
forenheith <6537537@gmail.com>
2017-03-15 02:02:32 +0300

    + GenreManagmentViewModel

---
+++ PVT.Q1.2017.Shop/PVT.Q1.2017.Shop/Areas/Management/Models/GenreManagmentViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace PVT.Q1._2017.Shop.Areas.Management.Models
+{
+    using global::Shop.Common.Models;
+
+    /// <summary>
+    /// </summary>
+    public class GenreManagmentViewModel
+    {
+        /// <summary>
+        /// Gets or sets the track.
+        /// </summary>
+        public Genre Genre { get; set; }
+    }
+}
