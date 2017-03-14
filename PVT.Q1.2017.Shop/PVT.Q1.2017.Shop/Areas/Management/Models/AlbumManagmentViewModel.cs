966aa2e87ec5e753e0eaa64bdf61b728fd3ea17b
forenheith <6537537@gmail.com>
2017-03-15 02:02:08 +0300

    + AlbumManagmentViewController

---
+++ PVT.Q1.2017.Shop/PVT.Q1.2017.Shop/Areas/Management/Models/AlbumManagmentViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace PVT.Q1._2017.Shop.Areas.Management.Models
+{
+    using global::Shop.Common.Models;
+
+    /// <summary>
+    /// </summary>
+    public class AlbumManagmentViewModel
+    {
+        /// <summary>
+        /// Gets or sets the track.
+        /// </summary>
+        public Album Album { get; set; }
+    }
+}
