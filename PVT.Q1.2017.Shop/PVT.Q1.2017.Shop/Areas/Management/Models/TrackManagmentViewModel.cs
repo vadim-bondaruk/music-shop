f74b2300220e77c2758fee117c1ad839d09c3498
forenheith <6537537@gmail.com>
2017-03-15 02:02:49 +0300

    * TRackManagmentViewModel

--- PVT.Q1.2017.Shop/PVT.Q1.2017.Shop/Areas/Management/Models/TrackManagmentViewModel.cs
+++ PVT.Q1.2017.Shop/PVT.Q1.2017.Shop/Areas/Management/Models/TrackManagmentViewModel.cs
@@ -1,29 +1,11 @@
 ﻿namespace PVT.Q1._2017.Shop.Areas.Management.Models
 {
-    using System;
-    using System.Collections.Generic;
-
     using global::Shop.Common.Models;
 
     /// <summary>
     /// </summary>
     public class TrackManagmentViewModel
     {
-        /// <summary>
-        ///     Gets or sets the album.
-        /// </summary>
-        public Album Album { get; set; }
-
-        /// <summary>
-        ///     Gets or sets the artist.
-        /// </summary>
-        public Artist Artist { get; set; }
-
-        /// <summary>
-        ///     Gets or sets the duration.
-        /// </summary>
-        public TimeSpan Duration { get; set; }
-
         /// <summary>
         /// Gets or sets the track.
         /// </summary>
