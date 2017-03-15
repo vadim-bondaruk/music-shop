55c9666696ac7324270325340fa32832b4869300
forenheith <6537537@gmail.com>
2017-03-15 16:23:22 +0300

    * fixed conflicts

--- PVT.Q1.2017.Shop/PVT.Q1.2017.Shop.Tests/AlbumPriceServiceTest.cs
+++ PVT.Q1.2017.Shop/PVT.Q1.2017.Shop.Tests/AlbumPriceServiceTest.cs
@@ -1,34 +1,49 @@
-﻿using Shop.DAL.Infrastruture;
-
-namespace PVT.Q1._2017.Shop.Tests
+﻿namespace PVT.Q1._2017.Shop.Tests
 {
     using System;
     using System.Linq;
     using System.Linq.Expressions;
-    using global::Moq;
+
     using global::Shop.BLL.Services;
     using global::Shop.BLL.Services.Infrastructure;
     using global::Shop.Common.Models;
+    using global::Shop.DAL.Infrastruture;
     using global::Shop.Infrastructure.Models;
-    using Microsoft.VisualStudio.TestTools.UnitTesting;
-    using Mocks;
 
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    using Moq;
+
+    using PVT.Q1._2017.Shop.Tests.Mocks;
+
+    /// <summary>
+    /// </summary>
     [TestClass]
     public class AlbumPriceServiceTest
     {
+        /// <summary>
+        /// </summary>
         private readonly IAlbumPriceService _albumPriceService;
+
+        /// <summary>
+        /// </summary>
         private readonly IRepositoryFactory _factory;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AlbumPriceServiceTest"/> class.
+        /// </summary>
         public AlbumPriceServiceTest()
         {
-            _factory = new RepositoryFactoryMoq();
-            _albumPriceService = new AlbumPriceService(_factory);
+            this._factory = new RepositoryFactoryMoq();
+            this._albumPriceService = new AlbumPriceService(this._factory);
         }
 
+        /// <summary>
+        /// </summary>
         [TestMethod]
         public void AddAlbumPricesTest()
         {
-            using (var repository = _factory.GetAlbumPriceRepository())
+            using (var repository = this._factory.GetAlbumPriceRepository())
             {
                 repository.AddOrUpdate(new AlbumPrice { Price = 9.99m });
                 repository.SaveChanges();
@@ -37,30 +52,35 @@ namespace PVT.Q1._2017.Shop.Tests
             }
         }
 
+        /// <summary>
+        /// </summary>
         [TestMethod]
         public void GetAlbumPriceTest()
         {
-            AddAlbumPricesTest();
-            Assert.IsNotNull(_albumPriceService.GeAlbumPrice(new Album(), new PriceLevel(), new Currency()));
+            this.AddAlbumPricesTest();
+            Assert.IsNotNull(this._albumPriceService.GeAlbumPrice(new Album(), new PriceLevel(), new Currency()));
 
-            Mock.Get(_factory.GetAlbumPriceRepository())
+            Mock.Get(this._factory.GetAlbumPriceRepository())
                 .Verify(
-                        m =>
-                            m.GetAll(It.IsAny<Expression<Func<AlbumPrice, bool>>>(),
-                                     It.IsAny<Expression<Func<AlbumPrice, BaseEntity>>[]>()), Times.Once);
+                    m =>
+                        m.GetAll(
+                            It.IsAny<Expression<Func<AlbumPrice, bool>>>(),
+                            It.IsAny<Expression<Func<AlbumPrice, BaseEntity>>[]>()),
+                    Times.Once);
         }
 
+        /// <summary>
+        /// </summary>
         [TestMethod]
         public void GetTrackPriceInfoTest()
         {
-            AddAlbumPricesTest();
-            Assert.IsNotNull(_albumPriceService.GetAlbumPriceInfo(1));
+            this.AddAlbumPricesTest();
+            Assert.IsNotNull(this._albumPriceService.GetAlbumPriceInfo(1));
 
-            Mock.Get(_factory.GetAlbumPriceRepository())
+            Mock.Get(this._factory.GetAlbumPriceRepository())
                 .Verify(
-                        m =>
-                            m.GetById(It.IsAny<int>(),
-                                      It.IsAny<Expression<Func<AlbumPrice, BaseEntity>>[]>()), Times.Once);
+                    m => m.GetById(It.IsAny<int>(), It.IsAny<Expression<Func<AlbumPrice, BaseEntity>>[]>()),
+                    Times.Once);
         }
     }
 }
