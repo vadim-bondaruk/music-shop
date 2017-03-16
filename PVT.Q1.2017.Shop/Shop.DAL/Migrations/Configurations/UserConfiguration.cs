55ad674671cc5cc5fab329ed4ee9845914d2ff00
Andrei Kukharenko <carbon_kas@mail.ru>
2017-03-17 00:27:24 +0300

    * restored userDB configuration

--- PVT.Q1.2017.Shop/Shop.DAL/Migrations/Configurations/UserConfiguration.cs
+++ PVT.Q1.2017.Shop/Shop.DAL/Migrations/Configurations/UserConfiguration.cs
@@ -10,12 +10,22 @@
     public class UserConfiguration : EntityTypeConfiguration<User>
     {
         /// <summary>
-        /// Initializes a new instance of the <see cref="UserConfiguration"/> class.
+        /// 
         /// </summary>
         public UserConfiguration()
         {
             HasKey(t => t.Id);
             Property(a => a.Id).HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
+            Property(p => p.BirthDate).IsOptional();
+            Property(p => p.Country).IsOptional().HasMaxLength(15);
+            Property(p => p.Email).IsRequired().HasMaxLength(40);
+            Property(p => p.FirstName).IsRequired().HasMaxLength(15).IsUnicode();
+            Property(p => p.LastName).IsRequired().HasMaxLength(25).IsUnicode();
+            Property(p => p.Login).IsRequired().HasMaxLength(15).IsUnicode();
+            Property(p => p.Password).IsRequired().HasMaxLength(50);
+            Property(p => p.PhoneNumber).IsOptional().HasMaxLength(30);
+            Property(p => p.Sex).IsRequired();
+            Property(p => p.UserRoles).IsRequired();
 
             ToTable("tbUsers");
         }
