4dabcb9603b026149b36c9cc0ec14b31dbf47569
forenheith <6537537@gmail.com>
2017-03-17 12:54:27 +0300

    + ArtistService

---
+++ PVT.Q1.2017.Shop/Shop.BLL/Services/ArtistService.cs
@@ -0,0 +1,79 @@
+﻿namespace Shop.BLL.Services
+{
+    using System;
+    using System.Collections.Generic;
+
+    using AutoMapper;
+
+    using Shop.BLL.Services.Infrastructure;
+    using Shop.Common.Models;
+    using Shop.Common.Models.ViewModels;
+    using Shop.DAL.Infrastruture;
+
+    /// <summary>
+    ///     The Artist service.
+    /// </summary>
+    public class ArtistService : BaseService, IArtistService
+    {
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="ArtistService" /> class.
+        /// </summary>
+        /// <param name="factory">
+        ///     The repository factory.
+        /// </param>
+        public ArtistService(IRepositoryFactory factory)
+            : base(factory)
+        {
+        }
+
+        /// <summary>
+        /// </summary>
+        /// <param name="id">
+        ///     The id.
+        /// </param>
+        /// <returns>
+        /// </returns>
+        public ArtistDetailsViewModel GetArtistInfo(int id)
+        {
+            using (var repository = this.Factory.GetArtistRepository())
+            {
+                var artist = repository.GetById(id);
+                if (artist == null)
+                {
+                    return null;
+                }
+
+                var viewModel = Mapper.Map<ArtistDetailsViewModel>(artist);
+                return viewModel;
+            }
+        }
+
+        /// <summary>
+        ///     Returns all registered Artists.
+        /// </summary>
+        /// <returns>
+        ///     All registered Artists.
+        /// </returns>
+        public ICollection<Artist> GetArtistsList()
+        {
+            using (var repository = this.Factory.GetArtistRepository())
+            {
+                return repository.GetAll();
+            }
+        }
+
+        /// <summary>
+        /// </summary>
+        /// <param name="artistId">
+        ///     The artist id.
+        /// </param>
+        /// <returns>
+        /// </returns>
+        /// <exception cref="NotImplementedException">
+        /// </exception>
+        public ICollection<Track> GetTracksList(int artistId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ PVT.Q1.2017.Shop/Shop.BLL/Services/Infrastructure/IArtistService.cs
@@ -0,0 +1,39 @@
+﻿namespace Shop.BLL.Services.Infrastructure
+{
+    using System.Collections.Generic;
+
+    using Shop.Common.Models;
+    using Shop.Common.Models.ViewModels;
+
+    /// <summary>
+    ///     The Artist service.
+    /// </summary>
+    public interface IArtistService
+    {
+        /// <summary>
+        ///     Returns the Artist with the specified <paramref name="id" />.
+        /// </summary>
+        /// <param name="id">The Artist id.</param>
+        /// <returns>
+        ///     The Artist with the specified <paramref name="id" /> or <b>null</b> if Artist doesn't exist.
+        /// </returns>
+        ArtistDetailsViewModel GetArtistInfo(int id);
+
+        /// <summary>
+        ///     Returns all registered Artists.
+        /// </summary>
+        /// <returns>
+        ///     All registered Artists.
+        /// </returns>
+        ICollection<Artist> GetArtistsList();
+
+        /// <summary>
+        /// </summary>
+        /// <param name="artistId">
+        /// The artist id.
+        /// </param>
+        /// <returns>
+        /// </returns>
+        ICollection<Track> GetTracksList(int artistId);
+    }
+}
