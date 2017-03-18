7d66dcb068eacfd2d465eb891d48b0872de68fa4
forenheith <6537537@gmail.com>
2017-03-18 11:10:33 +0300

    * Mapping

--- PVT.Q1.2017.Shop/Shop.BLL/DefaultModelsMapper.cs
+++ PVT.Q1.2017.Shop/Shop.BLL/DefaultModelsMapper.cs
@@ -15,6 +15,14 @@
         public static void MapModels()
         {
             Mapper.Initialize(cfg => cfg.CreateMap<Album, AlbumViewModel>());
+
+            Mapper.Initialize(
+                cfg =>
+                    cfg.CreateMap<ArtistDetailsViewModel, Artist>()
+                        .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Name))
+                        .ForMember(dest => dest.Birthday, opt => opt.MapFrom(src => src.Birthday))
+                        .ForMember(dest => dest.Biography, opt => opt.MapFrom(src => src.Biography))
+                        .ForMember(dest => dest.Photo, opt => opt.ResolveUsing(src => src.Photo)));
         }
     }
 }
