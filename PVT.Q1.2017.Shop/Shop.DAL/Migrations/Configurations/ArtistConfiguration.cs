e5773fc5e1f167334329cce99322b1ddc0576494
forenheith <6537537@gmail.com>
2017-03-18 11:08:00 +0300

    * fix configurations

--- PVT.Q1.2017.Shop/Shop.DAL/Migrations/Configurations/ArtistConfiguration.cs
+++ PVT.Q1.2017.Shop/Shop.DAL/Migrations/Configurations/ArtistConfiguration.cs
@@ -1,7 +1,6 @@
 ﻿namespace Shop.DAL.Migrations.Configurations
 {
     using System.ComponentModel.DataAnnotations.Schema;
-    using System.Data.Entity.Infrastructure.Annotations;
     using System.Data.Entity.ModelConfiguration;
 
     using Shop.Common.Models;
@@ -18,15 +17,11 @@
         {
             this.HasKey(t => t.Id);
             this.Property(a => a.Id).HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
-            this.Property(t => t.Name)
-                .IsRequired()
-                .HasMaxLength(150)
-                .IsUnicode()
-                .IsVariableLength()
-                .HasColumnAnnotation(
-                    "Index",
-                    new IndexAnnotation(new[] { new IndexAttribute("UniqueArtistName_Index") { IsUnique = true } }));
-            
+            this.Property(t => t.Name).IsRequired().HasMaxLength(150).IsUnicode().IsVariableLength();
+
+            // .HasColumnAnnotation(
+            // "Index",
+            // new IndexAnnotation(new[] { new IndexAttribute("UniqueArtistName_Index") { IsUnique = true } }));
             this.Property(a => a.Biography).IsMaxLength();
             this.Property(a => a.Birthday).IsOptional();
             this.ToTable("tbArtists");
