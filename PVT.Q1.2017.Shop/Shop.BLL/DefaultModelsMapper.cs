a67a84d2f3386ba163e3c0e74b7ff99248901d66
forenheith <6537537@gmail.com>
2017-03-20 09:48:13 +0300

    * DefaultModelMapper

--- PVT.Q1.2017.Shop/Shop.BLL/DefaultModelsMapper.cs
+++ PVT.Q1.2017.Shop/Shop.BLL/DefaultModelsMapper.cs
@@ -1,35 +1,36 @@
 ﻿namespace Shop.BLL
 {
     using AutoMapper;
-    using Common.Models;
-    using Common.Models.ViewModels;
+
+    using Shop.Common.Models;
+    using Shop.Common.Models.ViewModels;
 
     /// <summary>
-    /// Default models mapper.
+    ///     Default models mapper.
     /// </summary>
     public static class DefaultModelsMapper
     {
         /// <summary>
-        /// Maps models and view-models.
+        ///     Maps models and view-models.
         /// </summary>
         public static void MapModels()
         {
-            Mapper.Initialize(cfg => cfg.CreateMap<Album, AlbumViewModel>());
-
             Mapper.Initialize(
                 cfg =>
-                    cfg.CreateMap<ArtistManageViewModel, Artist>()
-                        .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Name))
-                        .ForMember(dest => dest.Birthday, opt => opt.MapFrom(src => src.Birthday))
-                        .ForMember(dest => dest.Biography, opt => opt.MapFrom(src => src.Biography))
-                        .ForMember(dest => dest.Photo, opt => opt.ResolveUsing(src => src.Photo)));
+                    {
+                        cfg.CreateMap<ArtistManageViewModel, Artist>()
+                            .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id))
+                            .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Name))
+                            .ForMember(dest => dest.Birthday, opt => opt.MapFrom(src => src.Birthday))
+                            .ForMember(dest => dest.Biography, opt => opt.MapFrom(src => src.Biography))
+                            .ReverseMap();
 
-            Mapper.Initialize(
-               cfg =>
-                   cfg.CreateMap<AlbumManageViewModel, Album>()
-                       .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Name))
-                       .ForMember(dest => dest.ReleaseDate, opt => opt.MapFrom(src => src.ReleaseDate))
-                       .ForMember(dest => dest.Cover, opt => opt.ResolveUsing(src => src.Cover)));
+                        cfg.CreateMap<AlbumManageViewModel, Album>()
+                            .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Name))
+                            .ForMember(dest => dest.ReleaseDate, opt => opt.MapFrom(src => src.ReleaseDate))
+                            .ForMember(dest => dest.Cover, opt => opt.ResolveUsing(src => src.Cover))
+                            .ReverseMap();
+                    });
         }
     }
 }
