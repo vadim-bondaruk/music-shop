b044e2849636d783632dd25d45eb438825e8ff63
forenheith <6537537@gmail.com>
2017-03-20 22:44:21 +0300

    * Actualize IArtistService

--- PVT.Q1.2017.Shop/Shop.BLL/Services/Infrastructure/IArtistService.cs
+++ PVT.Q1.2017.Shop/Shop.BLL/Services/Infrastructure/IArtistService.cs
@@ -1,5 +1,6 @@
 ﻿namespace Shop.BLL.Services.Infrastructure
 {
+    using Shop.Common.Models;
     using Shop.Common.Models.ViewModels;
 
     /// <summary>
@@ -8,19 +9,26 @@
     public interface IArtistService
     {
         /// <summary>
+        /// </summary>
+        /// <param name="artist">
+        /// The artist.
+        /// </param>
+        void Delete(Artist artist);
+
+        /// <summary>
         ///     Returns the Artist with the specified <paramref name="id" />.
         /// </summary>
         /// <param name="id">The Artist id.</param>
         /// <returns>
         ///     The Artist with the specified <paramref name="id" /> or <b>null</b> if Artist doesn't exist.
         /// </returns>
-        ArtistManageViewModel GetArtistViewModel(int id);
+        ArtistManageViewModel GetById(int id);
 
         /// <summary>
         /// </summary>
         /// <param name="viewModel">
-        /// The view model.
+        ///     The view model.
         /// </param>
-        int SaveNewArtist(ArtistManageViewModel viewModel);
+        int Save(ArtistManageViewModel viewModel);
     }
 }
