1f6483c8d9abe6b7fc96e1c28abbb06de8a6390e
forenheith <6537537@gmail.com>
2017-03-22 14:22:06 +0300

    * openfile script

--- PVT.Q1.2017.Shop/PVT.Q1.2017.Shop/Scripts/openfile.dialog.js
+++ PVT.Q1.2017.Shop/PVT.Q1.2017.Shop/Scripts/openfile.dialog.js
@@ -2,8 +2,8 @@
     // handling input changes
     $("#file").change(function () {
         // if file attached - save path to var
-        var fileResult = $(this).val();
+        var fileResult = $(this).val().replace("C:\\fakepath\\", "");
         // send value to fake input
-        $(this).parent().find("#photo-input").val(fileResult);
+        $(this).parent().find("#file-path").val(fileResult);
     });
 });
