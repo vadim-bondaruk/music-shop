e4bdebc277081f2dbf5c652fcd44873b14556980
forenheith <6537537@gmail.com>
2017-03-24 13:29:22 +0300

    * fix db state

--- PVT.Q1.2017.Shop/Shop.DAL/Migrations/201703241012324_Artists.Designer.cs
+++ PVT.Q1.2017.Shop/Shop.DAL/Migrations/201703241012324_Artists.Designer.cs
@@ -7,13 +7,13 @@ namespace Shop.DAL.Migrations
     using System.Resources;
     
     [GeneratedCode("EntityFramework.Migrations", "6.1.3-40302")]
-    public sealed partial class TablePrefixRemoved : IMigrationMetadata
+    public sealed partial class Artists : IMigrationMetadata
     {
-        private readonly ResourceManager Resources = new ResourceManager(typeof(TablePrefixRemoved));
+        private readonly ResourceManager Resources = new ResourceManager(typeof(Artists));
         
         string IMigrationMetadata.Id
         {
-            get { return "201703180835104_TablePrefixRemoved"; }
+            get { return "201703241012324_Artists"; }
         }
         
         string IMigrationMetadata.Source
