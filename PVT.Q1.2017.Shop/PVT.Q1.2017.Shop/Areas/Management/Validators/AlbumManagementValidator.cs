f2ed3afdc84fa80a349ba6f92b58f82be940edc3
forenheith <6537537@gmail.com>
2017-03-24 10:48:48 +0300

    * fix stylecops errors

--- PVT.Q1.2017.Shop/PVT.Q1.2017.Shop/Areas/Management/Validators/AlbumManagementValidator.cs
+++ PVT.Q1.2017.Shop/PVT.Q1.2017.Shop/Areas/Management/Validators/AlbumManagementValidator.cs
@@ -1,20 +1,21 @@
 ﻿namespace PVT.Q1._2017.Shop.Areas.Management.Validators
 {
     using FluentValidation;
-    using ViewModels;
+
+    using global::Shop.Common.Models.ViewModels;
 
     /// <summary>
-    /// The <see cref="AlbumManagementViewModel"/> validator.
+    ///     The <see cref="AlbumManagementViewModel" /> validator.
     /// </summary>
     public class AlbumManagementValidator : AbstractValidator<AlbumManagementViewModel>
     {
         /// <summary>
-        /// Initializes a new instance of the <see cref="AlbumManagementValidator"/> class.
+        ///     Initializes a new instance of the <see cref="AlbumManagementValidator" /> class.
         /// </summary>
         public AlbumManagementValidator()
         {
-            RuleFor(a => a.Name).NotNull().WithMessage("Укажите название альбома");
-            RuleFor(a => a.Name).Matches(@"^\S+(\s\S+)*$").WithMessage("Название альбома указано не корректно");
+            this.RuleFor(a => a.Name).NotNull().WithMessage("Укажите название альбома");
+            this.RuleFor(a => a.Name).Matches(@"^\S+(\s\S+)*$").WithMessage("Название альбома указано не корректно");
         }
     }
 }
--- PVT.Q1.2017.Shop/PVT.Q1.2017.Shop/Areas/Management/Validators/TrackManagementValidator.cs
+++ PVT.Q1.2017.Shop/PVT.Q1.2017.Shop/Areas/Management/Validators/TrackManagementValidator.cs
@@ -1,25 +1,26 @@
 ﻿namespace PVT.Q1._2017.Shop.Areas.Management.Validators
 {
     using FluentValidation;
-    using ViewModels;
+
+    using global::Shop.Common.Models.ViewModels;
 
     /// <summary>
-    /// The <see cref="TrackManagementViewModel"/> validator
+    ///     The <see cref="TrackManagementViewModel" /> validator
     /// </summary>
     public class TrackManagementValidator : AbstractValidator<TrackManagementViewModel>
     {
         /// <summary>
-        /// Initializes a new instance of the <see cref="TrackManagementValidator"/> class.
+        ///     Initializes a new instance of the <see cref="TrackManagementValidator" /> class.
         /// </summary>
         public TrackManagementValidator()
         {
-            RuleFor(t => t.Name).NotEmpty().WithMessage("Укажите название трека");
-            RuleFor(t => t.Name).Matches(@"^\S+(\s\S+)*$").WithMessage("Название трека указано не корректно");
+            this.RuleFor(t => t.Name).NotEmpty().WithMessage("Укажите название трека");
+            this.RuleFor(t => t.Name).Matches(@"^\S+(\s\S+)*$").WithMessage("Название трека указано не корректно");
 
-            RuleFor(t => t.TrackFile).NotNull().WithMessage("Укажите файл трека");
+            this.RuleFor(t => t.TrackFile).NotNull().WithMessage("Укажите файл трека");
 
-            RuleFor(t => t.Artist).NotNull().WithMessage("Не указан исполнитель трека");
-            RuleFor(t => t.Genre).NotNull().WithMessage("Не указан жанр трека");
+            this.RuleFor(t => t.Artist).NotNull().WithMessage("Не указан исполнитель трека");
+            this.RuleFor(t => t.Genre).NotNull().WithMessage("Не указан жанр трека");
         }
     }
 }
