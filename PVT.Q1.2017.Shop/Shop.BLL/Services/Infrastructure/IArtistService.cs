b8eecea13bf0dab1a73e0d3e56ebf2880b9eb4ec
forenheith <6537537@gmail.com>
2017-03-25 21:33:27 +0300

    * IArtistService

--- PVT.Q1.2017.Shop/Shop.BLL/Services/Infrastructure/IArtistService.cs
+++ PVT.Q1.2017.Shop/Shop.BLL/Services/Infrastructure/IArtistService.cs
@@ -9,26 +9,12 @@
     public interface IArtistService
     {
         /// <summary>
-        /// </summary>
-        /// <param name="artist">
-        /// The artist.
-        /// </param>
-        void Delete(Artist artist);
-
-        /// <summary>
         ///     Returns the Artist with the specified <paramref name="id" />.
         /// </summary>
         /// <param name="id">The Artist id.</param>
         /// <returns>
         ///     The Artist with the specified <paramref name="id" /> or <b>null</b> if Artist doesn't exist.
         /// </returns>
-        ArtistManagementViewModel GetById(int id);
-
-        /// <summary>
-        /// </summary>
-        /// <param name="viewModel">
-        ///     The view model.
-        /// </param>
-        int Save(ArtistManagementViewModel viewModel);
+        ArtistDetailsViewModel GetArtistDetails(int id);
     }
 }
