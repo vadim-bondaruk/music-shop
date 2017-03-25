94a3971b869e7f294f0e05983ad80e1226e62ca5
Aliaksei Padabed <Aliaksei.Padabed@gmail.com>
2017-03-25 20:40:03 +0300

    * mocked FirstOrDefault method for the repositories

--- PVT.Q1.2017.Shop/PVT.Q1.2017.Shop.Tests/CartControllerTests.cs
+++ PVT.Q1.2017.Shop/PVT.Q1.2017.Shop.Tests/CartControllerTests.cs
@@ -38,6 +38,7 @@ namespace PVT.Q1._2017.Shop.Tests
 
             Mock<ICartRepository> moqCartRepository = new Mock<ICartRepository>();
             moqCartRepository.Setup(m => m.GetAll(It.IsAny<Expression<Func<Cart, bool>>>())).Returns(carts);
+            moqCartRepository.Setup(m => m.FirstOrDefault(It.IsAny<Expression<Func<Cart, bool>>>())).Returns(() => carts.FirstOrDefault());
             moqCartRepository.Setup(m => m.GetById(It.Is<int>(t => t == 1))).Returns(cart);
 
             Mock<ICartService> moqCartService = new Mock<ICartService>();
