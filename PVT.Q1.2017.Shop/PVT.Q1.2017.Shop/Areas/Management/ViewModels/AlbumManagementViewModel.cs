f56c94d2b1409201d08b85cfd1129d1e27e203b0
forenheith <6537537@gmail.com>
2017-03-25 21:25:56 +0300

    * change view models location

---
+++ PVT.Q1.2017.Shop/PVT.Q1.2017.Shop/Areas/Management/ViewModels/AlbumManagementViewModel.cs
@@ -0,0 +1,38 @@
+﻿namespace PVT.Q1._2017.Shop.Areas.Management.ViewModels
+{
+    using System;
+    using System.Web;
+
+    using global::Shop.Common.Models;
+
+    /// <summary>
+    ///     The album view model.
+    /// </summary>
+    public class AlbumManagementViewModel
+    {
+        /// <summary>
+        /// Gets or sets the artist.
+        /// </summary>
+        public Artist Artist { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the album cover.
+        /// </summary>
+        public HttpPostedFileBase Cover { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the album name.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the album release date.
+        /// </summary>
+        public DateTime? ReleaseDate { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the album price.
+        /// </summary>
+        public HttpPostedFileBase UploadedImage { get; set; }
+    }
+}
---
+++ PVT.Q1.2017.Shop/PVT.Q1.2017.Shop/Areas/Management/ViewModels/ArtistManagementViewModel.cs
@@ -0,0 +1,50 @@
+﻿namespace PVT.Q1._2017.Shop.Areas.Management.ViewModels
+{
+    using System;
+    using System.ComponentModel;
+    using System.ComponentModel.DataAnnotations;
+    using System.Web;
+
+    /// <summary>
+    /// </summary>
+    public class ArtistManagementViewModel
+    {
+        /// <summary>
+        ///     Gets or sets the biography.
+        /// </summary>
+        public string Biography { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the birthday.
+        /// </summary>
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd MMMM yyyy}г.", ApplyFormatInEditMode = true)]
+        public DateTime? Birthday { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the id.
+        /// </summary>
+        public int? Id { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the name.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets the photo.
+        /// </summary>
+        public byte[] Photo { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the photo.
+        /// </summary>
+        public HttpPostedFileBase PostedPhoto { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the value.
+        /// </summary>
+        [Bindable(true)]
+        public virtual string Value { get; set; }
+    }
+}
