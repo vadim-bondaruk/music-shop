b3b7dc4835c0ba7635f7704b4111c60e98a9a54a
forenheith <6537537@gmail.com>
2017-03-25 21:32:26 +0300

    * ManagementMapper

--- PVT.Q1.2017.Shop/PVT.Q1.2017.Shop/Areas/Management/Helpers/ManagementMapper.cs
+++ PVT.Q1.2017.Shop/PVT.Q1.2017.Shop/Areas/Management/Helpers/ManagementMapper.cs
@@ -1,24 +1,27 @@
 ﻿namespace PVT.Q1._2017.Shop.Areas.Management.Helpers
 {
     using System.Web;
+
     using AutoMapper;
-    using Extensions;
+
     using global::Shop.Common.Models;
     using global::Shop.Common.Models.ViewModels;
-    using ViewModels;
+
+    using PVT.Q1._2017.Shop.Areas.Management.Extensions;
+    using PVT.Q1._2017.Shop.Areas.Management.ViewModels;
 
     /// <summary>
-    /// The management mapper
+    ///     The management mapper
     /// </summary>
     internal static class ManagementMapper
     {
         /// <summary>
-        /// The mapper for models with detailed information.
+        ///     The mapper for models with detailed information.
         /// </summary>
         private static readonly IMapper ManagementModelsMapper;
 
         /// <summary>
-        /// Initializes static members of the <see cref="ManagementMapper"/> class.
+        ///     Initializes static members of the <see cref="ManagementMapper" /> class.
         /// </summary>
         static ManagementMapper()
         {
@@ -26,41 +29,25 @@
         }
 
         /// <summary>
-        /// Executes a mapping from the <see cref="TrackManagementViewModel"/> model to a new <see cref="Track"/> model.
-        /// </summary>
-        /// <param name="trackManagementViewModel">
-        /// The track management view model.
-        /// </param>
-        /// <returns>
-        /// A new <see cref="Track"/> DTO model.
-        /// </returns>
-        public static Track GetTrackModel(TrackManagementViewModel trackManagementViewModel)
-        {
-            return ManagementModelsMapper.Map<Track>(trackManagementViewModel);
-        }
-
-        /// <summary>
-        /// Executes a mapping from the <see cref="TrackDetailsViewModel"/> model to a new <see cref="TrackManagementViewModel"/> model.
-        /// </summary>
-        /// <param name="track">
-        /// The track domain model.
-        /// </param>
-        /// <returns>
-        /// A new <see cref="TrackManagementViewModel"/> model.
-        /// </returns>
-        public static TrackManagementViewModel GetTrackManagementViewModel(TrackDetailsViewModel track)
-        {
-            return ManagementModelsMapper.Map<TrackManagementViewModel>(track);
-        }
-
-        /// <summary>
-        /// Executes a mapping from the <see cref="AlbumManagementViewModel"/> model to a new <see cref="Album"/> model.
         /// </summary>
         /// <param name="album">
-        /// The album management view model.
+        ///     The album.
         /// </param>
         /// <returns>
-        /// A new <see cref="Album"/> DTO model.
+        /// </returns>
+        public static AlbumManagementViewModel GetAlbumManagementViewModel(Album album)
+        {
+            return ManagementModelsMapper.Map<AlbumManagementViewModel>(album);
+        }
+
+        /// <summary>
+        ///     Executes a mapping from the <see cref="AlbumManagementViewModel" /> model to a new <see cref="Album" /> model.
+        /// </summary>
+        /// <param name="album">
+        ///     The album management view model.
+        /// </param>
+        /// <returns>
+        ///     A new <see cref="Album" /> DTO model.
         /// </returns>
         public static Album GetAlbumModel(AlbumManagementViewModel album)
         {
@@ -68,49 +55,97 @@
         }
 
         /// <summary>
-        /// Executes a mapping from the <see cref="AlbumDetailsViewModel"/> model to a new <see cref="AlbumManagementViewModel"/> model.
         /// </summary>
         /// <param name="album">
-        /// The album domain model.
+        ///     The album.
         /// </param>
+        /// <param name="viewModel"></param>
         /// <returns>
-        /// A new <see cref="AlbumManagementViewModel"/> model.
         /// </returns>
-        public static AlbumManagementViewModel GetAlbumManagementViewModel(AlbumDetailsViewModel album)
+        public static ArtistManagementViewModel GetArtistManagementViewModel(ArtistDetailsViewModel viewModel)
         {
-            return ManagementModelsMapper.Map<AlbumManagementViewModel>(album);
+            return ManagementModelsMapper.Map<ArtistManagementViewModel>(viewModel);
         }
 
         /// <summary>
-        /// Configures and returns a new instance of the mapper for models which have a list of other models.
+        /// </summary>
+        /// <param name="viewModel">
+        ///     The view model.
+        /// </param>
+        /// <returns>
+        /// </returns>
+        public static Artist GetArtistModel(ArtistManagementViewModel viewModel)
+        {
+            return ManagementModelsMapper.Map<Artist>(viewModel);
+        }
+
+        /// <summary>
+        ///     Executes a mapping from the <see cref="TrackDetailsViewModel" /> model to a new
+        ///     <see cref="TrackManagementViewModel" /> model.
+        /// </summary>
+        /// <param name="track">
+        ///     The track domain model.
+        /// </param>
+        /// <returns>
+        ///     A new <see cref="TrackManagementViewModel" /> model.
+        /// </returns>
+        public static TrackManagementViewModel GetTrackManagementViewModel(TrackDetailsViewModel track)
+        {
+            return ManagementModelsMapper.Map<TrackManagementViewModel>(track);
+        }
+
+        /// <summary>
+        ///     Executes a mapping from the <see cref="TrackManagementViewModel" /> model to a new <see cref="Track" /> model.
+        /// </summary>
+        /// <param name="trackManagementViewModel">
+        ///     The track management view model.
+        /// </param>
+        /// <returns>
+        ///     A new <see cref="Track" /> DTO model.
+        /// </returns>
+        public static Track GetTrackModel(TrackManagementViewModel trackManagementViewModel)
+        {
+            return ManagementModelsMapper.Map<Track>(trackManagementViewModel);
+        }
+
+        /// <summary>
+        ///     Configures and returns a new instance of the mapper for models which have a list of other models.
         /// </summary>
         /// <returns>
-        /// A new instance of the mapper for models which have a list of other models.
+        ///     A new instance of the mapper for models which have a list of other models.
         /// </returns>
         private static IMapper CreateManagementMapper()
         {
-            MapperConfiguration managementConfiguration = new MapperConfiguration(cfg =>
-            {
-                cfg.CreateMap<ArtistManagementViewModel, Artist>();
-                cfg.CreateMap<GenreViewModel, Genre>();
+            var managementConfiguration = new MapperConfiguration(
+                cfg =>
+                    {
+                        cfg.CreateMap<GenreViewModel, Genre>();
 
-                cfg.CreateMap<TrackDetailsViewModel, TrackManagementViewModel>()
-                                            .ForMember(dest => dest.Image, opt => opt.UseValue<HttpPostedFileBase>(null))
-                                            .ForMember(dest => dest.TrackFile, opt => opt.UseValue<HttpPostedFileBase>(null));
+                        cfg.CreateMap<TrackDetailsViewModel, TrackManagementViewModel>()
+                            .ForMember(dest => dest.Image, opt => opt.UseValue<HttpPostedFileBase>(null))
+                            .ForMember(dest => dest.TrackFile, opt => opt.UseValue<HttpPostedFileBase>(null));
 
-                cfg.CreateMap<TrackManagementViewModel, Track>()
-                                            .ForMember(dest => dest.Artist, opt => opt.MapFrom(src => src.Artist))
-                                            .ForMember(dest => dest.Genre, opt => opt.MapFrom(src => src.Genre))
-                                            .ForMember(dest => dest.TrackFile, opt => opt.ResolveUsing(src => src.TrackFile.ToBytes()))
-                                            .ForMember(dest => dest.Image, opt => opt.ResolveUsing(src => src.Image.ToBytes()));
+                        cfg.CreateMap<TrackManagementViewModel, Track>()
+                            .ForMember(dest => dest.Artist, opt => opt.MapFrom(src => src.Artist))
+                            .ForMember(dest => dest.Genre, opt => opt.MapFrom(src => src.Genre))
+                            .ForMember(dest => dest.TrackFile, opt => opt.ResolveUsing(src => src.TrackFile.ToBytes()))
+                            .ForMember(dest => dest.Image, opt => opt.ResolveUsing(src => src.Image.ToBytes()));
 
-                cfg.CreateMap<AlbumDetailsViewModel, AlbumManagementViewModel>()
-                                            .ForMember(dest => dest.Cover, opt => opt.UseValue<HttpPostedFileBase>(null));
+                        cfg.CreateMap<AlbumDetailsViewModel, Album>()
+                            .ForMember(dest => dest.Cover, opt => opt.UseValue<HttpPostedFileBase>(null));
 
-                cfg.CreateMap<AlbumManagementViewModel, Album>()
-                                            .ForMember(dest => dest.Artist, opt => opt.MapFrom(src => src.Artist))
-                                            .ForMember(dest => dest.Cover, opt => opt.ResolveUsing(src => src.Cover.ToBytes()));
-            });
+                        cfg.CreateMap<AlbumManagementViewModel, Album>()
+                            .ForMember(dest => dest.Artist, opt => opt.MapFrom(src => src.Artist))
+                            .ForMember(dest => dest.Cover, opt => opt.ResolveUsing(src => src.Cover.ToBytes()))
+                            .ReverseMap();
+
+                        cfg.CreateMap<ArtistManagementViewModel, Artist>()
+                            .ForMember(dest => dest.Photo, opt => opt.ResolveUsing(src => src.Photo))
+                             .ForMember(dest => dest.Photo, opt => opt.ResolveUsing(src => src.PostedPhoto.ToBytes()));
+
+                        cfg.CreateMap<ArtistDetailsViewModel, ArtistManagementViewModel>()
+                            .ForMember(dest => dest.Photo, opt => opt.MapFrom(src => src.Photo));
+                    });
 
             return managementConfiguration.CreateMapper();
         }
