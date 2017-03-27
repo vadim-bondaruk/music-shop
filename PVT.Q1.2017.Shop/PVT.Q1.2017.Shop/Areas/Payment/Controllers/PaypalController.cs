3ae1a3516f87a986df42b588685480ce36f889ea
Aliaksandr Kaptsiuh <Cheater-xg@yandex.ru>
2017-03-27 16:09:26 +0300

    + added views for success and failure payment operations * updated PaymentService for work with CartViewModel

--- PVT.Q1.2017.Shop/PVT.Q1.2017.Shop/Areas/Payment/Controllers/PaypalController.cs
+++ PVT.Q1.2017.Shop/PVT.Q1.2017.Shop/Areas/Payment/Controllers/PaypalController.cs
@@ -1,10 +1,11 @@
-﻿using Shop.BLL.Services;
-using Shop.BLL.Services.Infrastructure;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Shop.BLL.Services;
+using Shop.BLL.Services.Infrastructure;
+using Shop.Common.ViewModels;
 
 namespace PVT.Q1._2017.Shop.Areas.Payment.Controllers
 {
@@ -17,22 +18,35 @@ namespace PVT.Q1._2017.Shop.Areas.Payment.Controllers
             this._paymentService = paymentService;
         }
 
-        // GET: Payment/Paypal
+        [HttpGet]
         public ActionResult Index()
         {
             return View();
         }
 
-        // GET: Payment/Paypal
+        [HttpGet]
+        public ActionResult Success()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Failure()
+        {
+            return View();
+        }
+
+        [HttpGet]
         public ActionResult PaymentWithCreditCard()
         {
             var viewName = _paymentService.CreatePaymentWithCreditCard();
             return Content(viewName); // View("SuccessView");
         }
 
-        public ActionResult PaymentWithPaypal()
+        [HttpPost]
+        public ActionResult PaymentWithPaypalDemo()
         {
-            var content = _paymentService.PaymentWithPaypal(this.Request, this.Session);
+            var content = _paymentService.PaymentWithPaypalDemo(this.Request, this.Session);
 
             if (content.StartsWith("http"))
             {
@@ -40,6 +54,29 @@ namespace PVT.Q1._2017.Shop.Areas.Payment.Controllers
             }
                 
             return Content(content);
+        }
+
+        [HttpPost]
+        public ActionResult PaymentWithPaypal(CartViewModel cart)
+        {
+            if (cart == null)
+            {
+                return RedirectToAction("Failure");
+            }
+            var status = _paymentService.PaymentWithPaypal(this.Request, this.Session, cart);
+
+            if (status.StartsWith("http"))
+            {
+                return Redirect(status);
+            }
+            if(status == "Success")
+            {
+                return RedirectToAction("Success");
+            }
+            else
+            {
+                return RedirectToAction("Failure");
+            }
         }
     }
 }
