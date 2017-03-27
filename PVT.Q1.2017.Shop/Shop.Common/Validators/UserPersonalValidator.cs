6854cc4326e6bee138923c646c44f15fe7896f7a
Andrei Kukharenko <carbon_kas@mail.ru>
2017-03-27 15:49:12 +0300

    * validation for registration and change personal data

--- PVT.Q1.2017.Shop/Shop.Common/Validators/UserPersonalValidator.cs
+++ PVT.Q1.2017.Shop/Shop.Common/Validators/UserPersonalValidator.cs
@@ -5,7 +5,7 @@
     using PVT.Q1._2017.Shop.ViewModels;
 
     /// <summary>
-    /// Defines rules for user registration
+    /// Defines rules for UserPersonalViewModel (change personal data in account)
     /// </summary>
     public class UserPersonalValidator : AbstractValidator<UserPersonalViewModel>
     {
@@ -16,12 +16,19 @@
         {
             RuleFor(u => u.FirstName).NotEmpty()
                 .WithMessage("Поле обязательно должно быть заполнено");
+            RuleFor(u => u.FirstName).Matches("^[a-zA-Zа-яА-Я]*$")
+               .WithMessage("Используйте только буквы");
 
             RuleFor(u => u.LastName).NotEmpty()
                 .WithMessage("Поле обязательно должно быть заполнено");
+            RuleFor(u => u.LastName).Matches("^[a-zA-Zа-яА-Я]*$")
+                .WithMessage("Используйте только буквы");
 
-            RuleFor(u => u.BirthDate).InclusiveBetween(DateTime.Today.AddYears(-80), DateTime.Today)
-                .WithMessage("Дата рождения выбрана неверно");
+            RuleFor(u => u.BirthDate).ExclusiveBetween(DateTime.Today.AddYears(-80), DateTime.Today.AddYears(-5))
+                .WithMessage("Дата рождения выбрана некорректно");
+
+            RuleFor(u => u.Country).Matches("^[a-zA-Zа-яА-Я]*$")
+               .WithMessage("Используйте только буквы");
 
             RuleFor(u => u.PhoneNumber)
                 .Matches(@"^(\s*)?(\+)?([- _():=+]?\d[- _():=+]?){10,14}(\s*)?$")
