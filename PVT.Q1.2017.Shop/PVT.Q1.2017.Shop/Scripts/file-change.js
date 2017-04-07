df7f2b2d44473b48a8b8e68beb6debad4edcccd4
forenheith <6537537@gmail.com>
2017-04-07 10:26:41 +0300

    * js optimized

--- PVT.Q1.2017.Shop/PVT.Q1.2017.Shop/Scripts/file-change.js
+++ PVT.Q1.2017.Shop/PVT.Q1.2017.Shop/Scripts/file-change.js
@@ -6,8 +6,9 @@
 });
 
 $(document).ready(function () {
-    $("#track-file").change(function (event) {
-        var output = document.getElementById("track-file-name");
-        output.src = URL.createObjectURL(event.target.files[0]);
+    $("#track-file").change(function () {
+        var fileResult = $(this).val().replace("C:\\fakepath\\", "");
+        $(this).parent().find("#file-path").val(fileResult);
+        $(this).parent().find("#audio").src= fileResult;
     });
 });
--- PVT.Q1.2017.Shop/PVT.Q1.2017.Shop/Scripts/openfile.dialog.js
+++ PVT.Q1.2017.Shop/PVT.Q1.2017.Shop/Scripts/openfile.dialog.js
@@ -1,7 +1,1 @@
-﻿$(document).ready(function() {
-    // handling input changes
-    $("#track-file").change(function () {
-        var fileResult = $(this).val().replace("C:\\fakepath\\", "");
-        $(this).parent().find("#file-path").val(fileResult);
-    });
-});
+﻿
