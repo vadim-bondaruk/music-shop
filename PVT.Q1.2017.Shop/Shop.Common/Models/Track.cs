8c9d0e2cab0a6139b95f3c618e6abc9aef5a8c3b
forenheith <6537537@gmail.com>
2017-04-11 11:33:09 +0300

    + FileName field

--- PVT.Q1.2017.Shop/Shop.Common/Models/Track.cs
+++ PVT.Q1.2017.Shop/Shop.Common/Models/Track.cs
@@ -2,99 +2,106 @@
 {
     using System;
     using System.Collections.Generic;
+
     using FluentValidation.Attributes;
-    using Infrastructure.Models;
-    using Validators;
+
+    using Shop.Common.Validators;
+    using Shop.Infrastructure.Models;
 
     /// <summary>
-    /// The track.
+    ///     The track.
     /// </summary>
     [Validator(typeof(TrackValidator))]
     public class Track : BaseEntity
     {
         /// <summary>
-        /// Gets or sets the name.
-        /// </summary>
-        public string Name { get; set; }
-
-        /// <summary>
-        /// Gets or sets the track release date.
-        /// </summary>
-        public DateTime? ReleaseDate { get; set; }
-
-        /// <summary>
-        /// The track image. Optional.
-        /// </summary>
-        public byte[] Image { get; set; }
-
-        /// <summary>
-        /// The track file. Required. We are selling it!
-        /// </summary>
-        public byte[] TrackFile { get; set; }
-
-        /// <summary>
-        /// The track sample. Optional.
-        /// </summary>
-        public byte[] TrackSample { get; set; }
-
-        /// <summary>
-        /// Gets or sets the track duration.
-        /// </summary>
-        public TimeSpan? Duration { get; set; }
-
-        /// <summary>
-        /// Gets or sets the artist id.
-        /// </summary>
-        public int ArtistId { get; set; }
-
-        /// <summary>
-        /// Gets or sets the genre id.
-        /// </summary>
-        public int GenreId { get; set; }
-
-        /// <summary>
-        /// The owner id.
-        /// </summary>
-        public int? OwnerId { get; set; }
-
-        /// <summary>
-        /// Gets or sets the artist.
-        /// </summary>
-        public virtual Artist Artist { get; set; }
-
-        /// <summary>
-        /// Gets or sets the genre.
-        /// </summary>
-        public virtual Genre Genre { get; set; }
-
-        /// <summary>
-        /// The owner of the track.
-        /// </summary>
-        public virtual User Owner { get; set; }
-
-        /// <summary>
-        /// Gets or sets the albums which contain the current track.
+        ///     Gets or sets the albums which contain the current track.
         /// </summary>
         public virtual ICollection<AlbumTrackRelation> Albums { get; set; }
 
         /// <summary>
-        /// Gets or sets the track prices.
+        ///     Gets or sets the artist.
         /// </summary>
-        public virtual ICollection<TrackPrice> TrackPrices { get; set; }
+        public virtual Artist Artist { get; set; }
 
         /// <summary>
-        /// Gets or sets all user votes for the current track.
+        ///     Gets or sets the artist id.
         /// </summary>
-        public virtual ICollection<Vote> Votes { get; set; }
+        public int ArtistId { get; set; }
 
         /// <summary>
-        /// Gets or sets all user feedbacks the current track.
+        ///     Carts with this Track
+        /// </summary>
+        public virtual ICollection<OrderTrack> Carts { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the track duration.
+        /// </summary>
+        public TimeSpan? Duration { get; set; }
+
+        /// <summary>
+        ///     Gets or sets all user feedbacks the current track.
         /// </summary>
         public virtual ICollection<Feedback> Feedbacks { get; set; }
 
         /// <summary>
-        /// Carts with this Track
+        /// Gets or sets the file name.
         /// </summary>
-        public virtual ICollection<OrderTrack> Carts { get; set; }
+        public string FileName { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the genre.
+        /// </summary>
+        public virtual Genre Genre { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the genre id.
+        /// </summary>
+        public int GenreId { get; set; }
+
+        /// <summary>
+        ///     The track image. Optional.
+        /// </summary>
+        public byte[] Image { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the name.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        ///     The owner of the track.
+        /// </summary>
+        public virtual User Owner { get; set; }
+
+        /// <summary>
+        ///     The owner id.
+        /// </summary>
+        public int? OwnerId { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the track release date.
+        /// </summary>
+        public DateTime? ReleaseDate { get; set; }
+
+        /// <summary>
+        ///     The track file. Required. We are selling it!
+        /// </summary>
+        public byte[] TrackFile { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the track prices.
+        /// </summary>
+        public virtual ICollection<TrackPrice> TrackPrices { get; set; }
+
+        /// <summary>
+        ///     The track sample. Optional.
+        /// </summary>
+        public byte[] TrackSample { get; set; }
+
+        /// <summary>
+        ///     Gets or sets all user votes for the current track.
+        /// </summary>
+        public virtual ICollection<Vote> Votes { get; set; }
     }
 }
