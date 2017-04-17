63e84ba9b6271590b9341244b74f60f9972a0681
Aliaksandr Kaptsiuh <Cheater-xg@yandex.ru>
2017-04-18 00:42:57 +0300

    * updated model for payment transaction info
    Former-commit-id: 0bfdb9cedc137f564e546971eafa27e42989eea2

--- PVT.Q1.2017.Shop/Shop.Common/Models/PaymentTransaction.cs
+++ PVT.Q1.2017.Shop/Shop.Common/Models/PaymentTransaction.cs
@@ -19,19 +19,33 @@
         public int Amount { get; set; }
 
         /// <summary>
+        /// User ID
+        /// </summary>
+        public int UserId { get; set; }
+
+        /// <summary>
+        /// Currency ID
+        /// </summary>
+        public int CurrencyId { get; set; }
+        
+        /// <summary>
         /// Transaction holder
         /// </summary>
-        public User User { get; set; }
+        public virtual UserData User { get; set; }
 
         /// <summary>
         /// Currency for transaction
         /// </summary>
-        public Currency Currency { get; set; }
+        public virtual Currency Currency { get; set; }
 
         /// <summary>
-        /// The order ID for which the transaction was created
+        /// Purchased tracks
         /// </summary>
-        public int OrderId { get; set; }
+        public virtual ICollection<Track> Tracks { get; set; }
 
+        /// <summary>
+        /// Purchased albums
+        /// </summary>
+        public virtual ICollection<Album> Albums { get; set; }
     }
 }
