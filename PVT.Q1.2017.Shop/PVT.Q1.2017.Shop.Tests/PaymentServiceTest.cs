702d8052643b591600d602341afaab2282524947
Aliaksandr Kaptsiuh <Cheater-xg@yandex.ru>
2017-04-17 18:07:11 +0300

    + payment service tests added
    Former-commit-id: 9bf49319b89e184ce6d7196b630d823009bcc99b

---
+++ PVT.Q1.2017.Shop/PVT.Q1.2017.Shop.Tests/PaymentServiceTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Shop.BLL.Services.Infrastructure;
+using Shop.DAL.Infrastruture;
+using PVT.Q1._2017.Shop.Tests.Moq;
+using Shop.BLL.Services;
+
+namespace PVT.Q1._2017.Shop.Tests
+{
+    /// <summary>
+    /// Summary description for PaymentServiceTest
+    /// </summary>
+    [TestClass]
+    public class PaymentServiceTest
+    {
+
+        private readonly IPaymentService _paymentService;
+        private readonly IRepositoryFactory _factory;
+      
+        public PaymentServiceTest()
+        {
+            _factory = new RepositoryFactoryMoq();
+            _paymentService = new PaymentService(_factory);
+        }
+               
+        [TestMethod]
+        public void TestPaymentWithPayPal_InvalidArgument()
+        {
+            var result = _paymentService.PaymentWithPaypal(null, null);
+            Assert.AreSame("Failure", result);
+
+        }
+    }
+}
