a6945320e6fcea50bd4c148249318d5d6e8113f7
forenheith <6537537@gmail.com>
2017-04-19 12:05:53 +0300

    + startpage news
    Former-commit-id: 72462e7f1c5c0bcdbec0150c259b332421ded5a1
    Former-commit-id: 0f5b52f201e7389222aea15cfaf118a357d22f5b

---
+++ PVT.Q1.2017.Shop/PVT.Q1.2017.Shop/Areas/News/Controllers/MusicController.cs
@@ -0,0 +1,19 @@
+﻿namespace PVT.Q1._2017.Shop.Areas.News.Controllers
+{
+    using System.Web.Mvc;
+
+    /// <summary>
+    /// </summary>
+    public class NewsController : Controller
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Article()
+        {
+            this.ViewBag.Title = "Новости музыки";
+            return this.View("_NewAlbumOfDorn");
+        }
+    }
+}
