c2e24a9a9695d8666000d2a462ccaeeae0f7f37a
Aliaksei Padabed <aliaksei.padabed@gmail.com>
2017-04-21 10:11:21 +0300

    * implemted some async methods in TrackService (incompleted)
    Former-commit-id: 10e02279430a0aca942cc5084cc907860d0b0f30
    Former-commit-id: bd1d5e78c4c5fc01b91b3997b9316e9f74e8bb82

--- PVT.Q1.2017.Shop/Shop.BLL/Services/Infrastructure/ITrackService.cs
+++ PVT.Q1.2017.Shop/Shop.BLL/Services/Infrastructure/ITrackService.cs
@@ -1,6 +1,8 @@
 ﻿namespace Shop.BLL.Services.Infrastructure
 {
     using System.Collections.Generic;
+    using System.Threading.Tasks;
+
     using Common.ViewModels;
     using Shop.Infrastructure.Models;
 
@@ -42,7 +44,7 @@
         /// <returns>
         /// All registered tracks.
         /// </returns>
-        ICollection<TrackViewModel> GetTracks(int? currencyCode = null, int? priceLevel = null, int? userId = null);
+        Task<ICollection<TrackViewModel>> GetTracksAsync(int? currencyCode = null, int? priceLevel = null, int? userId = null);
 
         /// <summary>
         /// Returns all registered tracks using the specified currency and price level for track price.
@@ -82,7 +84,7 @@
         /// <returns>
         /// All registered tracks with detailed information.
         /// </returns>
-        ICollection<TrackDetailsViewModel> GetDetailedTracksList(int? currencyCode = null, int? priceLevel = null, int? userId = null);
+        Task<ICollection<TrackDetailsViewModel>> GetDetailedTracksListAsync(int? currencyCode = null, int? priceLevel = null, int? userId = null);
 
         /// <summary>
         /// Returns all registered tracks with detailed information using the specified currency and price level for track price.
@@ -171,7 +173,7 @@
         /// <returns>
         /// All tracks that the specified user have bought.
         /// </returns>
-        ICollection<PurchasedTrackViewModel> GetPurchasedTracks(int userId);
+        Task<ICollection<PurchasedTrackViewModel>> GetPurchasedTracksAsync(int userId);
 
         /// <summary>
         /// Return all tracks that the specified user have bought.
