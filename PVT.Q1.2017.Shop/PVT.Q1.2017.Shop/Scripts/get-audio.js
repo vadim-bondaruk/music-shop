218cbd8eff11c124f4059e0c2735854e168f2052
forenheith <6537537@gmail.com>
2017-04-22 21:17:59 +0300

    + added get-audio js script
    Former-commit-id: 7c6defd98d41c4d3d1a26326dd86b88a785bccb5
    Former-commit-id: e67227010e87343f8d9015ecace7c6afdbf0d417

---
+++ PVT.Q1.2017.Shop/PVT.Q1.2017.Shop/Scripts/get-audio.js
@@ -0,0 +1,22 @@
+﻿var currentId;
+function GetAudio(getUrl, id) {
+    $("#preview-btn").text(" Загрузка...");
+    $.ajax({
+        success: function() {
+            GetAudioPlayer(getUrl, id);
+        },
+        error: function(xhr) {
+            alert(xhr.responseText);
+        }
+    });
+}
+
+function GetAudioPlayer(url, id) {
+    var itemId = "#item-" + id;
+    $("#audio-hidden").insertAfter($(itemId).find($(".fa")));
+    $(currentId).find($(".fa")).show();
+    $(itemId).find($(".fa")).hide();
+    currentId = itemId;   
+    $("#audio-hidden").show();
+    $("#audio-hidden").attr({"src":url});
+}
