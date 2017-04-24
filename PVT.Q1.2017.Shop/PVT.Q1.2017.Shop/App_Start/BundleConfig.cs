fd7e5e3adbba4d52554a39be2596f1f5ffa4ed29
forenheith <6537537@gmail.com>
2017-04-24 15:24:51 +0300

    * searchbox deleted
    Former-commit-id: 8d944063b89bf43bc843df79f14432083e510cbc
    Former-commit-id: f5050f4ba24892cfbb61b31bb40bcc45d63348f0

--- PVT.Q1.2017.Shop/PVT.Q1.2017.Shop/App_Start/BundleConfig.cs
+++ PVT.Q1.2017.Shop/PVT.Q1.2017.Shop/App_Start/BundleConfig.cs
@@ -10,31 +10,32 @@
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include("~/Scripts/jquery-{version}.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include("~/Scripts/jquery.validate*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/inputmask").Include(
-            "~/Scripts/jquery.inputmask/inputmask.js",
-            "~/Scripts/jquery.inputmask/jquery.inputmask.js",
-            "~/Scripts/jquery.inputmask/inputmask.extensions.js",
-            "~/Scripts/jquery.inputmask/inputmask.date.extensions.js",
-            "~/Scripts/jquery.inputmask/inputmask.numeric.extensions.js"));
+            bundles.Add(
+                new ScriptBundle("~/bundles/inputmask").Include(
+                    "~/Scripts/jquery.inputmask/inputmask.js",
+                    "~/Scripts/jquery.inputmask/jquery.inputmask.js",
+                    "~/Scripts/jquery.inputmask/inputmask.extensions.js",
+                    "~/Scripts/jquery.inputmask/inputmask.date.extensions.js",
+                    "~/Scripts/jquery.inputmask/inputmask.numeric.extensions.js"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include("~/Scripts/modernizr-*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+            bundles.Add(
+                new ScriptBundle("~/bundles/bootstrap").Include("~/Scripts/bootstrap.js", "~/Scripts/respond.js"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/bootstrap.css", "~/Content/site.css"));
+
+            bundles.Add(
+                new StyleBundle("~/Content/cssTracks").Include(
+                    "~/Content/tracks.css",
+                    "~/Content/ratingStars.css",
+                    "~/Content/font-awesome.min.css"));
         }
     }
 }
