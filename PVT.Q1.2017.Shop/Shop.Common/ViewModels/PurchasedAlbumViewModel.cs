de8d96fd4e7db2388d9b660c4937568b27be9e03
Aliaksei Padabed <Aliaksei.Padabed@gmail.com>
2017-04-26 21:07:54 +0300

    * implemented GetPurchasedAlbums method * implemented GetPurchasedList action in AlbumsController * implemented PurchasedAlbumViewModel - removed unused files
    Former-commit-id: f3512fa03adc592178cd85ec6b64f2450982128e
    Former-commit-id: 620f74023b3f11033ec431f45c7808c1ff3941db

---
+++ PVT.Q1.2017.Shop/Shop.Common/ViewModels/PurchasedAlbumViewModel.cs
@@ -0,0 +1,58 @@
+﻿namespace Shop.Common.ViewModels
+{
+    using System.Collections.Generic;
+    using System.ComponentModel;
+
+    /// <summary>
+    /// The purchased album view model.
+    /// </summary>
+    public class PurchasedAlbumViewModel
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PurchasedAlbumViewModel"/> class.
+        /// </summary>
+        public PurchasedAlbumViewModel()
+        {
+            Tracks = new List<PurchasedTrackViewModel>();
+        }
+
+        /// <summary>
+        /// Album id.
+        /// </summary>
+        public int Id
+        {
+            get; set;
+        }
+
+        /// <summary>
+        /// Album name.
+        /// </summary>
+        [DisplayName("Название")]
+        public string Name
+        {
+            get; set;
+        }
+
+        /// <summary>
+        /// Album cover.
+        /// </summary>
+        public byte[] Cover
+        {
+            get; set;
+        }
+
+        /// <summary>
+        /// The Artist. Optional.
+        /// </summary>
+        [DisplayName("Исполнитель")]
+        public ArtistViewModel Artist
+        {
+            get; set;
+        }
+
+        /// <summary>
+        /// The tracks from the album.
+        /// </summary>
+        public ICollection<PurchasedTrackViewModel> Tracks { get; set; }
+    }
+}
