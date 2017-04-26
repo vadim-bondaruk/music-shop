5d9eaeb36a65c026bfa9873204a642c4679ee3bc
Aliaksei Padabed <Aliaksei.Padabed@gmail.com>
2017-04-26 09:14:13 +0300

    * fixed JavaScript bugs (validation configuration was called but required validator object was not found because of jquery.validate*.js file was moved to bundles and was not called on some pages)
    Former-commit-id: fab213013398a5f04e6002ea89f1fbdbf2204475
    Former-commit-id: c6f5abc9af59d6946bbc904742d5b2549399039e

--- PVT.Q1.2017.Shop/PVT.Q1.2017.Shop/Scripts/common.js
+++ PVT.Q1.2017.Shop/PVT.Q1.2017.Shop/Scripts/common.js
@@ -1,17 +1,4 @@
 ﻿$(function () {
-    $.validator.methods.date = function (value, element) {
-        return this.optional(element) || moment(value, "DD.MM.YYYY", true).isValid();
-    }
-
-    $.validator.methods.range = function (value, element, param) {
-        var globalizedValue = value.replace(",", ".");
-        return this.optional(element) || (globalizedValue >= param[0] && globalizedValue <= param[1]);
-    }
-
-    $.validator.methods.number = function (value, element) {
-        return this.optional(element) || /^-?(?:\d+|\d{1,3}(?:[\s\.,]\d{3})+)(?:[\.,]\d+)?$/.test(value);
-    }
-
     var date = new Date();
     date.setDate(date.getDate());
     $(".datecontrol").datepicker({
@@ -28,6 +15,8 @@
         e.preventDefault();
         $(this).tab('show');
     });
+
+    updateOrdersCount();
 });
 
 function addAlbumToCart(sender, albumId, text, alternateText) {
@@ -139,7 +128,3 @@ function updateOrdersCount() {
 function getCurrentLocationRoot() {
     return window.location.origin;
 }
-
-$(function() {
-    updateOrdersCount();
-});
