4b3bcad4711e49bb241f19e6f7311242d2901bd3
Vadim Bondaruk <wadbondaruk@gmail.com>
2017-04-28 01:11:21 +0300

    * fix is user exist
    Former-commit-id: ea4acba65e4fa23ff0a7a1d54ebfd0e634c1fb76
    Former-commit-id: 27bdf7f4203e0b5a2cbda2780f0655877030a5b9

--- PVT.Q1.2017.Shop/Shop.Common/Validators/ChangeLoginValidator.cs
+++ PVT.Q1.2017.Shop/Shop.Common/Validators/ChangeLoginValidator.cs
@@ -1,6 +1,8 @@
 ﻿namespace Shop.Common.Validators
 {
     using FluentValidation;
+    using Infrastructure;
+    using System.Web.Mvc;
     using ViewModels;
 
     /// <summary>
@@ -13,12 +15,16 @@
         /// </summary>
         public ChangeLoginValidator()
         {
+            var validator = DependencyResolver.Current.GetService<IUserValidator>();
+
             RuleFor(u => u.Login).NotEmpty()
                 .WithMessage("Поле обязательно должно быть заполнено");
             RuleFor(u => u.Login).Matches("^[a-zA-Z0-9_.-]*$")
                 .WithMessage("Только буквы латинского алфавита, цифры и знак подчеркивания");
             RuleFor(u => u.Login).Length(4, 50)
                .WithMessage("Логин должен содержать не менее 4 символов");
+            RuleFor(u => u.Login).Must(l => !validator.IsUserExist(l))
+                .WithMessage("Пользователь с таким логином уже существует");
         }
     }
 }
