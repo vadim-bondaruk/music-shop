6a0610f28e048b561e1a0b9601583b82ea3e4edc
Vadim Bondaruk <wadbondaruk@gmail.com>
2017-04-28 01:16:04 +0300

    * fix date input
    Former-commit-id: 8a33f2ce64dd6d9d8356250f9e4e8842e398e526
    Former-commit-id: cf37f523a83994d592448cf9c80a8101fd4e153d

--- PVT.Q1.2017.Shop/Shop.Common/Validators/UserEditValidator.cs
+++ PVT.Q1.2017.Shop/Shop.Common/Validators/UserEditValidator.cs
@@ -26,6 +26,9 @@
             RuleFor(u => u.BirthDate).Must(c => c != null ?c <= DateTime.Now.AddYears(-5) : true)
                 .WithMessage("Пользователю должно быть минимум 5 лет");
 
+            RuleFor(u => u.BirthDate).Must(c => c != null ? c >= DateTime.Now.AddYears(-130) : true)
+                .WithMessage("Максимальный возраст 130 лет");
+
             RuleFor(u => u.PhoneNumber)
                 .Matches(@"^(\s*)?(\+)?([- _():=+]?\d[- _():=+]?){12,14}(\s*)?$")
                 .WithMessage("Некорректный номер");
